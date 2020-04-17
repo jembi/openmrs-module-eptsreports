3071507e5400a2fce1fd9afb0a02fba13188d67f
cfaife <clerio.faife@gmail.com>
2020-04-17 16:41:34 +0200

    MEPTS-446: mvn clean install

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/HivCohortQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/HivCohortQueries.java
@@ -377,137 +377,133 @@ public class HivCohortQueries {
 
   private CohortDefinition getPatientsTransferredOutOrSuspended(
       int transferedOutOrSuspendedConcept, int patientStateId) {
-	  SqlCohortDefinition sqlCohortDefinition = new SqlCohortDefinition();
+    SqlCohortDefinition sqlCohortDefinition = new SqlCohortDefinition();
 
-	    sqlCohortDefinition.setName("get Patients With Most Recent Date Have Fila or Consultation ");
- 	    sqlCohortDefinition.addParameter(new Parameter("onOrBefore", "onOrBefore", Date.class));
-	    sqlCohortDefinition.addParameter(new Parameter("location", "Location", Location.class));
+    sqlCohortDefinition.setName("get Patients With Most Recent Date Have Fila or Consultation ");
+    sqlCohortDefinition.addParameter(new Parameter("onOrBefore", "onOrBefore", Date.class));
+    sqlCohortDefinition.addParameter(new Parameter("location", "Location", Location.class));
 
-	    Map<String, Integer> map = new HashMap<>();
-	    map.put(
-	        "adultoSeguimentoEncounterType",
-	        hivMetadata.getAdultoSeguimentoEncounterType().getEncounterTypeId());
-	    map.put(
-	        "pediatriaSeguimentoEncounterType",
-	        hivMetadata.getPediatriaSeguimentoEncounterType().getEncounterTypeId());
-	    map.put(
-	        "pharmaciaEncounterType", hivMetadata.getARVPharmaciaEncounterType().getEncounterTypeId());
-	    map.put(
-	        "masterCardDrugPickupEncounterType",
-	        hivMetadata.getMasterCardDrugPickupEncounterType().getEncounterTypeId());
-	    map.put("artDatePickup", hivMetadata.getArtDatePickupMasterCard().getConceptId());
-	    map.put(
-	        "masterCardEncounterType", hivMetadata.getMasterCardEncounterType().getEncounterTypeId());
-	    map.put(
-	        "stateOfStayOfPreArtPatient", hivMetadata.getStateOfStayOfPreArtPatient().getConceptId());
-	    map.put("transferredOutConcept", hivMetadata.getTransferredOutConcept().getConceptId());
-	    map.put("autoTransferConcept", hivMetadata.getAutoTransferConcept().getConceptId());
-	    map.put("stateOfStayOfArtPatient", hivMetadata.getStateOfStayOfArtPatient().getConceptId());
-	    map.put("artProgram", hivMetadata.getARTProgram().getProgramId());
-	    map.put(
-	        "transferredOutToAnotherHealthFacilityWorkflowState",
-	        hivMetadata
-	            .getTransferredOutToAnotherHealthFacilityWorkflowState()
-	            .getProgramWorkflowStateId());
+    Map<String, Integer> map = new HashMap<>();
+    map.put(
+        "adultoSeguimentoEncounterType",
+        hivMetadata.getAdultoSeguimentoEncounterType().getEncounterTypeId());
+    map.put(
+        "pediatriaSeguimentoEncounterType",
+        hivMetadata.getPediatriaSeguimentoEncounterType().getEncounterTypeId());
+    map.put(
+        "pharmaciaEncounterType", hivMetadata.getARVPharmaciaEncounterType().getEncounterTypeId());
+    map.put(
+        "masterCardDrugPickupEncounterType",
+        hivMetadata.getMasterCardDrugPickupEncounterType().getEncounterTypeId());
+    map.put("artDatePickup", hivMetadata.getArtDatePickupMasterCard().getConceptId());
+    map.put(
+        "masterCardEncounterType", hivMetadata.getMasterCardEncounterType().getEncounterTypeId());
+    map.put(
+        "stateOfStayOfPreArtPatient", hivMetadata.getStateOfStayOfPreArtPatient().getConceptId());
+    map.put("transferredOutConcept", hivMetadata.getTransferredOutConcept().getConceptId());
+    map.put("autoTransferConcept", hivMetadata.getAutoTransferConcept().getConceptId());
+    map.put("stateOfStayOfArtPatient", hivMetadata.getStateOfStayOfArtPatient().getConceptId());
+    map.put("artProgram", hivMetadata.getARTProgram().getProgramId());
+    map.put(
+        "transferredOutToAnotherHealthFacilityWorkflowState",
+        hivMetadata
+            .getTransferredOutToAnotherHealthFacilityWorkflowState()
+            .getProgramWorkflowStateId());
 
-	    String query =
-	        "  SELECT mostrecent.patient_id "
-	            + "FROM ("
-	            + " SELECT lastest.patient_id ,Max(lastest.last_date) as  last_date "
-	            + " FROM (  "
-	            + "    SELECT p.patient_id ,ps.start_date AS last_date  "
-	            + "    FROM patient p   "
-	            + "        INNER JOIN patient_program pg   "
-	            + "            ON p.patient_id=pg.patient_id   "
-	            + "        INNER JOIN patient_state ps   "
-	            + "            ON pg.patient_program_id=ps.patient_program_id   "
-	            + "    WHERE pg.voided=0   "
-	            + "        AND ps.voided=0   "
-	            + "        AND p.voided=0   "
-	            + "        AND pg.program_id= ${artProgram}  "
-	            + "        AND ps.state = ${transferredOutToAnotherHealthFacilityWorkflowState}   "
-	            + "        AND ps.end_date is null   "
-	            + "        AND ps.start_date <= :onOrBefore   "
-	            + "        AND pg.location_id= :location   "
-	            
-	            + "  "
-	            + "    UNION  "
-	            + "  "
-	            + "    SELECT  p.patient_id,  o.obs_datetime AS last_date  "
-	            + "    FROM patient p    "
-	            + "        INNER JOIN encounter e   "
-	            + "            ON e.patient_id=p.patient_id   "
-	            + "        INNER JOIN obs o   "
-	            + "            ON o.encounter_id=e.encounter_id   "
-	            + "    WHERE  p.voided = 0   "
-	            + "        AND e.voided = 0   "
-	            + "        AND o.voided = 0   "
-	            + "        AND e.encounter_type = ${masterCardEncounterType}   "
-	            + "        AND o.concept_id = ${stateOfStayOfPreArtPatient}  "
-	            + "        AND o.value_coded =  ${transferredOutConcept}   "
-	            + "        AND o.obs_datetime <= :onOrBefore   "
-	            + "        AND e.location_id =  :location   "
-	          
-	            + "    UNION   "
-	            + "    SELECT  p.patient_id , e.encounter_datetime AS last_date  "
-	            + "    FROM patient p    "
-	            + "        INNER JOIN encounter e   "
-	            + "            ON e.patient_id=p.patient_id   "
-	            + "        INNER JOIN obs o   "
-	            + "            ON o.encounter_id=e.encounter_id   "
-	            + "    WHERE  p.voided = 0   "
-	            + "        AND e.voided = 0   "
-	            + "        AND o.voided = 0   "
-	            + "        AND e.encounter_type = ${adultoSeguimentoEncounterType}  "
-	            + "        AND o.concept_id = ${stateOfStayOfArtPatient}  "
-	            + "        AND o.value_coded = ${transferredOutConcept}   "
-	            + "        AND e.encounter_datetime <= :onOrBefore   "
-	            + "        AND e.location_id =  :location  "
-	         
-	             
-	            + ") lastest   "
-	            + "WHERE lastest.patient_id NOT  IN("
-	            + " "
-	            + "  			     SELECT  p.patient_id    "
-	            + "	                 FROM patient p      "
-	            + "	                     INNER JOIN encounter e     "
-	            + "	                         ON e.patient_id=p.patient_id     "
-	            + "	                 WHERE  p.voided = 0     "
-	            + "	                     AND e.voided = 0     "
-	            + "	                     AND e.encounter_type IN (${adultoSeguimentoEncounterType},"
-	            + "${pediatriaSeguimentoEncounterType},"
-	            + "${pharmaciaEncounterType})    "
-	            + "	                     AND e.encounter_datetime > lastest.last_date "
-	            + " AND e.encounter_datetime <=  :onOrBefore    "
-	            + "	                     AND e.location_id =  :location    "
-	            + "	                 GROUP BY p.patient_id "
-	            + " UNION "
-	            + "        			 SELECT  p.patient_id    "
-	            + "	                 FROM patient p       "
-	            + "	                      INNER JOIN encounter e      "
-	            + "	                          ON e.patient_id=p.patient_id      "
-	            + "	                      INNER JOIN obs o      "
-	            + "	                          ON o.encounter_id=e.encounter_id      "
-	            + "	                  WHERE  p.voided = 0      "
-	            + "	                      AND e.voided = 0      "
-	            + "	                      AND o.voided = 0      "
-	            + "	                      AND e.encounter_type = ${masterCardDrugPickupEncounterType}     "
-	            + "	                      AND o.concept_id = ${artDatePickup}     "
-	            + "	                      AND o.value_datetime > lastest.last_date  "
-	            + " AND o.value_datetime <= :onOrBefore      "
-	            + "	                      AND e.location_id =  :location     "
-	            + "	                  GROUP BY p.patient_id   "
-	            + ")  "
-	            + " GROUP BY lastest.patient_id"
-	            + " )mostrecent "
-	            + " GROUP BY mostrecent.patient_id";
+    String query =
+        "  SELECT mostrecent.patient_id "
+            + "FROM ("
+            + " SELECT lastest.patient_id ,Max(lastest.last_date) as  last_date "
+            + " FROM (  "
+            + "    SELECT p.patient_id ,ps.start_date AS last_date  "
+            + "    FROM patient p   "
+            + "        INNER JOIN patient_program pg   "
+            + "            ON p.patient_id=pg.patient_id   "
+            + "        INNER JOIN patient_state ps   "
+            + "            ON pg.patient_program_id=ps.patient_program_id   "
+            + "    WHERE pg.voided=0   "
+            + "        AND ps.voided=0   "
+            + "        AND p.voided=0   "
+            + "        AND pg.program_id= ${artProgram}  "
+            + "        AND ps.state = ${transferredOutToAnotherHealthFacilityWorkflowState}   "
+            + "        AND ps.end_date is null   "
+            + "        AND ps.start_date <= :onOrBefore   "
+            + "        AND pg.location_id= :location   "
+            + "  "
+            + "    UNION  "
+            + "  "
+            + "    SELECT  p.patient_id,  o.obs_datetime AS last_date  "
+            + "    FROM patient p    "
+            + "        INNER JOIN encounter e   "
+            + "            ON e.patient_id=p.patient_id   "
+            + "        INNER JOIN obs o   "
+            + "            ON o.encounter_id=e.encounter_id   "
+            + "    WHERE  p.voided = 0   "
+            + "        AND e.voided = 0   "
+            + "        AND o.voided = 0   "
+            + "        AND e.encounter_type = ${masterCardEncounterType}   "
+            + "        AND o.concept_id = ${stateOfStayOfPreArtPatient}  "
+            + "        AND o.value_coded =  ${transferredOutConcept}   "
+            + "        AND o.obs_datetime <= :onOrBefore   "
+            + "        AND e.location_id =  :location   "
+            + "    UNION   "
+            + "    SELECT  p.patient_id , e.encounter_datetime AS last_date  "
+            + "    FROM patient p    "
+            + "        INNER JOIN encounter e   "
+            + "            ON e.patient_id=p.patient_id   "
+            + "        INNER JOIN obs o   "
+            + "            ON o.encounter_id=e.encounter_id   "
+            + "    WHERE  p.voided = 0   "
+            + "        AND e.voided = 0   "
+            + "        AND o.voided = 0   "
+            + "        AND e.encounter_type = ${adultoSeguimentoEncounterType}  "
+            + "        AND o.concept_id = ${stateOfStayOfArtPatient}  "
+            + "        AND o.value_coded = ${transferredOutConcept}   "
+            + "        AND e.encounter_datetime <= :onOrBefore   "
+            + "        AND e.location_id =  :location  "
+            + ") lastest   "
+            + "WHERE lastest.patient_id NOT  IN("
+            + " "
+            + "  			     SELECT  p.patient_id    "
+            + "	                 FROM patient p      "
+            + "	                     INNER JOIN encounter e     "
+            + "	                         ON e.patient_id=p.patient_id     "
+            + "	                 WHERE  p.voided = 0     "
+            + "	                     AND e.voided = 0     "
+            + "	                     AND e.encounter_type IN (${adultoSeguimentoEncounterType},"
+            + "${pediatriaSeguimentoEncounterType},"
+            + "${pharmaciaEncounterType})    "
+            + "	                     AND e.encounter_datetime > lastest.last_date "
+            + " AND e.encounter_datetime <=  :onOrBefore    "
+            + "	                     AND e.location_id =  :location    "
+            + "	                 GROUP BY p.patient_id "
+            + " UNION "
+            + "        			 SELECT  p.patient_id    "
+            + "	                 FROM patient p       "
+            + "	                      INNER JOIN encounter e      "
+            + "	                          ON e.patient_id=p.patient_id      "
+            + "	                      INNER JOIN obs o      "
+            + "	                          ON o.encounter_id=e.encounter_id      "
+            + "	                  WHERE  p.voided = 0      "
+            + "	                      AND e.voided = 0      "
+            + "	                      AND o.voided = 0      "
+            + "	                      AND e.encounter_type = ${masterCardDrugPickupEncounterType}     "
+            + "	                      AND o.concept_id = ${artDatePickup}     "
+            + "	                      AND o.value_datetime > lastest.last_date  "
+            + " AND o.value_datetime <= :onOrBefore      "
+            + "	                      AND e.location_id =  :location     "
+            + "	                  GROUP BY p.patient_id   "
+            + ")  "
+            + " GROUP BY lastest.patient_id"
+            + " )mostrecent "
+            + " GROUP BY mostrecent.patient_id";
 
-	    StringSubstitutor stringSubstitutor = new StringSubstitutor(map);
-	    String mappedQuery = stringSubstitutor.replace(query);
+    StringSubstitutor stringSubstitutor = new StringSubstitutor(map);
+    String mappedQuery = stringSubstitutor.replace(query);
 
-	    sqlCohortDefinition.setQuery(mappedQuery);
+    sqlCohortDefinition.setQuery(mappedQuery);
 
-	    return sqlCohortDefinition;
+    return sqlCohortDefinition;
   }
 
   /**
