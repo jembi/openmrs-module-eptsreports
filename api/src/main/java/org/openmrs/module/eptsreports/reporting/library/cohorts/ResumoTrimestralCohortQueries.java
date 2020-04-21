1e5de406ed6a5bdb0d47aa1a9153e6f004495550
Yassin Nurmahomed <yassin.nurmahomed@jembi.org>
2020-04-21 11:08:57 +0200

    Fix parameters on G

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/ResumoTrimestralCohortQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/ResumoTrimestralCohortQueries.java
@@ -169,7 +169,7 @@ public class ResumoTrimestralCohortQueries {
     comp.addSearch("L", mapStraightThrough(indicatorL));
     comp.addSearch(
         "lastSecondTherapeuticLine",
-        map(lastSecondTherapeuticLine, "onOrBefore=${onOrBefore}, location=${location}"));
+        map(lastSecondTherapeuticLine, "onOrBefore=${onOrBefore},locationList=${location}"));
     comp.setCompositionString(
         "((A OR B) AND NOT (C OR I OR J OR L)) AND lastSecondTherapeuticLine");
     return comp;
