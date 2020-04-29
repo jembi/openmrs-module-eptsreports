e8258ddee8b2fec4460df5e0926068137affb499
Paulo Sérgio Jaime Matsinhe <psmatsinhe@gmail.com>
2020-04-29 14:26:28 +0200

    Revert "Fix G second therapeutic line query (#50)"
    This reverts commit 861e779a863e1518ad9700e929964050e2693bab.

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/ResumoTrimestralCohortQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/ResumoTrimestralCohortQueries.java
@@ -323,7 +323,7 @@ public class ResumoTrimestralCohortQueries {
     CodedObsCohortDefinition cd = new CodedObsCohortDefinition();
     cd.addParameter(new Parameter("onOrBefore", "End date", Date.class));
     cd.addParameter(new Parameter("locationList", "Location", Location.class));
-    cd.addEncounterType(hivMetadata.getAdultoSeguimentoEncounterType());
+    cd.addEncounterType(hivMetadata.getMasterCardEncounterType());
     cd.setTimeModifier(TimeModifier.LAST);
     cd.setQuestion(hivMetadata.getTherapeuticLineConcept());
     cd.setOperator(SetComparator.IN);
