699c4673cb9280a21baebc30213c862e2b54c1d0
cfaife <clerio.faife@gmail.com>
2020-05-26 12:28:58 +0200

    MEPTS-502: added  new calculation for  LTFU

---
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/calculation/ltfu/LTFUCalculation.java
@@ -0,0 +1,202 @@
+package org.openmrs.module.eptsreports.reporting.calculation.ltfu;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.apache.commons.text.StringSubstitutor;
+import org.openmrs.Concept;
+import org.openmrs.Encounter;
+import org.openmrs.EncounterType;
+import org.openmrs.Location;
+import org.openmrs.Obs;
+import org.openmrs.api.context.Context;
+import org.openmrs.calculation.patient.PatientCalculationContext;
+import org.openmrs.calculation.result.CalculationResultMap;
+import org.openmrs.calculation.result.SimpleResult;
+import org.openmrs.module.eptsreports.metadata.CommonMetadata;
+import org.openmrs.module.eptsreports.metadata.HivMetadata;
+import org.openmrs.module.eptsreports.reporting.calculation.AbstractPatientCalculation;
+import org.openmrs.module.eptsreports.reporting.calculation.BooleanResult;
+import org.openmrs.module.eptsreports.reporting.calculation.common.EPTSCalculationService;
+import org.openmrs.module.eptsreports.reporting.utils.EptsCalculationUtils;
+import org.openmrs.module.reporting.common.TimeQualifier;
+import org.openmrs.module.reporting.data.patient.definition.SqlPatientDataDefinition;
+import org.openmrs.module.reporting.evaluation.parameter.Parameter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LTFUCalculation extends AbstractPatientCalculation {
+
+	private static final String LOCATION = "location";
+
+	private static final String ON_OR_BEFORE = "onOrBefore";
+	
+	private static final String  NUM_DAYS = "numDays";
+
+	@Override
+	public CalculationResultMap evaluate(Collection<Integer> cohort, Map<String, Object> parameterValues,
+			PatientCalculationContext context) {
+
+		Location location = (Location) context.getFromCache(LOCATION);
+
+		Date onOrBefore = (Date) context.getFromCache(ON_OR_BEFORE);
+		
+		Integer numDays = (Integer)  parameterValues.get(NUM_DAYS);
+
+		HivMetadata hivMetadata = Context.getRegisteredComponents(HivMetadata.class).get(0);
+		CommonMetadata commonMetadata = Context.getRegisteredComponents(CommonMetadata.class).get(0);
+
+		EncounterType pharmacyEncounterType = hivMetadata.getARVPharmaciaEncounterType();
+		Concept returnVisitDateForArvDrugConcept = hivMetadata.getReturnVisitDateForArvDrugConcept();
+
+		EncounterType adultEncounterType = hivMetadata.getAdultoSeguimentoEncounterType();
+		EncounterType pediatricEncounterType = hivMetadata.getPediatriaSeguimentoEncounterType();
+		Concept returnVisitDateConcept = commonMetadata.getReturnVisitDateConcept();
+
+		EncounterType masterCardDrugPickupEncounterType = hivMetadata.getMasterCardDrugPickupEncounterType();
+		Concept artDatePickupMasterCard = hivMetadata.getArtDatePickupMasterCard();
+
+		EPTSCalculationService eptsCalculationService = Context.getRegisteredComponents(EPTSCalculationService.class)
+				.get(0);
+
+		CalculationResultMap lastPharmacyEncounterMao = eptsCalculationService.getEncounter(
+				Arrays.asList(pharmacyEncounterType), TimeQualifier.LAST, cohort, location, onOrBefore, context);
+
+		CalculationResultMap lastPediatricAdultEncounterMao = eptsCalculationService.getEncounter(
+				Arrays.asList(adultEncounterType, pediatricEncounterType), TimeQualifier.LAST, cohort, location,
+				onOrBefore, context);
+
+		CalculationResultMap getLastMasterCardPickUpMap = this
+				.getLastMasterCardPickUp(masterCardDrugPickupEncounterType, artDatePickupMasterCard, cohort, context);
+
+		CalculationResultMap map = new CalculationResultMap();
+
+		for (Integer patientId : cohort) {
+
+			SimpleResult simpleResultPharmacy = (SimpleResult) lastPharmacyEncounterMao.get(patientId);
+			SimpleResult simpleResultPediatricAdult = (SimpleResult) lastPediatricAdultEncounterMao.get(patientId);
+			
+			Date maxPharmacyDate = null, maxAdultPediatricDate =null, maxMasterCardPickupDate = null;
+
+			if (simpleResultPharmacy != null) {
+
+				Encounter encounter = (Encounter) simpleResultPharmacy.getValue();
+
+				if(hasTheEncounterHaveObs(encounter, returnVisitDateForArvDrugConcept)) {
+					maxPharmacyDate = encounter.getEncounterDatetime();
+				}
+
+			}
+			
+			if(simpleResultPediatricAdult!=null) {
+				
+				Encounter encounter = (Encounter) simpleResultPediatricAdult.getValue();
+				
+				if(hasTheEncounterHaveObs(encounter, returnVisitDateConcept)) {
+					maxAdultPediatricDate = encounter.getEncounterDatetime();
+				}
+	
+			}
+
+			maxMasterCardPickupDate = EptsCalculationUtils.resultForPatient(getLastMasterCardPickUpMap, patientId);
+			
+			List<Date> dates = Arrays.asList (maxPharmacyDate,maxAdultPediatricDate,maxMasterCardPickupDate);
+
+			Date date  = this.getTheMaxDate(dates);
+			
+			if(date == null) {
+				continue;
+			}
+			
+			Calendar calendar  = Calendar.getInstance();
+			calendar.setTime(date);
+			calendar.add(Calendar.DATE, numDays);
+			Date maxDate  = calendar.getTime();
+			
+			if(maxDate.compareTo(onOrBefore)<=0) {
+				map.put(patientId, new BooleanResult(true, this));
+			}
+
+		}
+		
+		
+
+		return map;
+	}
+
+	private CalculationResultMap getLastMasterCardPickUp(EncounterType encounterType, Concept concept,
+			Collection<Integer> cohort, PatientCalculationContext context) {
+
+		SqlPatientDataDefinition patientDataDefinition = new SqlPatientDataDefinition();
+		patientDataDefinition.addParameter(new Parameter("onOrBefore", "onOrBefore", Date.class));
+		patientDataDefinition.addParameter(new Parameter("location", "location", Location.class));
+		String sql = "SELECT enc.patient_id,   "
+				+ "                             Date_add(Max(obs.value_datetime), interval 30 day) value_datetime  "
+				+ "                         FROM   patient pa   "
+				+ "                             inner join encounter enc   "
+				+ "                                 ON enc.patient_id = pa.patient_id   "
+				+ "                             inner join obs obs   "
+				+ "                                 ON obs.encounter_id = enc.encounter_id   "
+				+ "                         WHERE  pa.voided = 0   "
+				+ "                             AND enc.voided = 0   "
+				+ "                             AND obs.voided = 0   "
+				+ "                             AND obs.concept_id = ${concept}   "
+				+ "                             AND obs.value_datetime IS NOT NULL   "
+				+ "                             AND enc.encounter_type = ${encounterType}   "
+				+ "                             AND enc.location_id = :location   "
+				+ "                             AND obs.value_datetime <= :onOrBefore  "
+				+ "                        GROUP  BY pa.patient_id  ";
+
+		Map<String, Integer> map = new HashMap<>();
+		map.put("encounterType", encounterType.getEncounterTypeId());
+		map.put("concept", concept.getConceptId());
+
+		StringSubstitutor sb = new StringSubstitutor(map);
+		String mappedQuery = sb.replace(sql);
+
+		patientDataDefinition.setQuery(mappedQuery);
+		Map<String, Object> params = new HashMap<>();
+		params.put("location", context.getFromCache("location"));
+		params.put("onOrBefore", context.getFromCache("onOrBefore"));
+		return EptsCalculationUtils.evaluateWithReporting(patientDataDefinition, cohort, params, null, context);
+	}
+
+	private boolean hasTheEncounterHaveObs(Encounter encounter, Concept concept) {
+		Set<Obs> obss = encounter.getObs();
+		for (Obs o : obss) {
+			if (o.getConcept().equals(concept)) {
+ 				return true;
+			}
+		}
+		return  false;
+		
+	}
+	
+	private Date getTheMaxDate(List<Date> dates) {
+		
+		 
+		Date max  = new GregorianCalendar(1980,5,1).getTime();
+		for(Date current:dates) {
+			if(current!=null) {
+				if(current.compareTo(max)>0) {
+					max = current;
+				}
+			}
+		}
+		
+		if(max.equals(new GregorianCalendar(1980,5,1).getTime())) {
+			return null;
+		}
+		
+		return  max;
+		
+	}
+
+}
