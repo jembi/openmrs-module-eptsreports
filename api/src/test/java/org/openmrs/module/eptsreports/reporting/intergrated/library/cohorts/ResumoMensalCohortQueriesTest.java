712ab8c575cf0273f3bb1fcbf143e2b9587e7afa
Simao Mazive <simao.mazive@jembi.org>
2020-07-17 15:23:52 +0200

    adds coverage for c1 indicator

--- api/src/test/java/org/openmrs/module/eptsreports/reporting/intergrated/library/cohorts/ResumoMensalCohortQueriesTest.java
+++ api/src/test/java/org/openmrs/module/eptsreports/reporting/intergrated/library/cohorts/ResumoMensalCohortQueriesTest.java
@@ -117,7 +117,7 @@ public class ResumoMensalCohortQueriesTest extends DefinitionsTest {
     assertEquals(1, evaluatedCohort.getMemberIds().size());
     assertTrue(evaluatedCohort.getMemberIds().contains(1933));
   }
-  
+
   @Test
   public void getPatientsWhoInitiatedPreTarvAtAfacilityDuringCurrentMonthA2()
       throws EvaluationException {
@@ -132,13 +132,11 @@ public class ResumoMensalCohortQueriesTest extends DefinitionsTest {
     EvaluatedCohort evaluatedCohort = evaluateCohortDefinition(cd, parameters);
 
     assertEquals(2, evaluatedCohort.getMemberIds().size());
-    System.out.println(evaluatedCohort.getMemberIds());
-    assertTrue(evaluatedCohort.getMemberIds().contains(41584));
+    assertTrue(evaluatedCohort.getMemberIds().contains(1003));
   }
-  
+
   @Test
-  public void getNumberOfPatientInitiedPreArtDuringCurrentMothA2()
-      throws EvaluationException {
+  public void getNumberOfPatientInitiedPreArtDuringCurrentMothA2() throws EvaluationException {
     CohortDefinition cd =
         resumoMensalCohortQueries.getNumberOfPatientInitiedPreArtDuringCurrentMothA2();
 
@@ -149,9 +147,8 @@ public class ResumoMensalCohortQueriesTest extends DefinitionsTest {
 
     EvaluatedCohort evaluatedCohort = evaluateCohortDefinition(cd, parameters);
 
-    assertEquals(3, evaluatedCohort.getMemberIds().size());
-    System.out.println(evaluatedCohort.getMemberIds());
-    assertTrue(evaluatedCohort.getMemberIds().contains(41584));
+    assertEquals(2, evaluatedCohort.getMemberIds().size());
+    assertTrue(evaluatedCohort.getMemberIds().contains(1003));
   }
 
   @Test
@@ -166,9 +163,7 @@ public class ResumoMensalCohortQueriesTest extends DefinitionsTest {
     parameters.put(new Parameter("location", "Location", Location.class), this.getLocation());
 
     EvaluatedCohort evaluatedCohort = evaluateCohortDefinition(cd, parameters);
-
-    assertEquals(1, evaluatedCohort.getMemberIds().size());
-    System.out.println(evaluatedCohort.getMemberIds());
-    assertTrue(evaluatedCohort.getMemberIds().contains(41584));
+    assertEquals(2, evaluatedCohort.getMemberIds().size());
+    assertTrue(evaluatedCohort.getMemberIds().contains(1003));
   }
 }
