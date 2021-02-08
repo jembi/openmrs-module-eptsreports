e51675a29c0069c221b3f29ef42c460140192d6a
cfaife <clerio.faife@gmail.com>
2021-02-08 14:03:48 +0200

    MEPTS-759: added the A from CXCA SCRN

---
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/CXCASCRNCohortQueries.java
@@ -0,0 +1,61 @@
+package org.openmrs.module.eptsreports.reporting.library.cohorts;
+
+import org.openmrs.Cohort;
+import org.openmrs.Location;
+import org.openmrs.module.eptsreports.reporting.utils.EptsReportUtils;
+import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
+import org.openmrs.module.reporting.cohort.definition.CompositionCohortDefinition;
+import org.openmrs.module.reporting.evaluation.parameter.Parameter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class CXCASCRNCohortQueries {
+
+    private TxCurrCohortQueries txCurrCohortQueries;
+
+    private GenderCohortQueries genderCohortQueries;
+
+    private AgeCohortQueries ageCohortQueries;
+
+
+    @Autowired
+    public  CXCASCRNCohortQueries(TxCurrCohortQueries txCurrCohortQueries,
+                                  GenderCohortQueries genderCohortQueries,
+                                  AgeCohortQueries ageCohortQueries){
+        this.txCurrCohortQueries = txCurrCohortQueries;
+        this.genderCohortQueries = genderCohortQueries;
+        this.ageCohortQueries=  ageCohortQueries;
+    }
+
+    /**
+     * A: Select all patients from Tx_curr by end of reporting period and who are female and Age >= 15 years
+     */
+
+    private CohortDefinition getA(){
+        CompositionCohortDefinition cd = new CompositionCohortDefinition();
+        cd.setName("A from  CXCA SCRN");
+        cd.addParameter(new Parameter("startDate","Start Date", Date.class));
+        cd.addParameter(new Parameter("endDate","End Date", Date.class));
+        cd.addParameter(new Parameter("location","Location", Location.class));
+
+        CohortDefinition txcurr = this.txCurrCohortQueries.getTxCurrCompositionCohort("txcurr",true);
+
+        CohortDefinition female = this.genderCohortQueries.femaleCohort();
+
+        CohortDefinition adults = this.ageCohortQueries.createXtoYAgeCohort("adullts",15,null);
+
+        cd.addSearch("txcurr", EptsReportUtils.map(txcurr,"onOrBefore=${endDate},location=${location}"));
+
+        cd.addSearch("female", EptsReportUtils.map(female,""));
+
+        cd.addSearch("adults", EptsReportUtils.map(adults,""));
+
+        cd.setCompositionString("txcurr AND female AND adults");
+
+    }
+
+
+}
