eb52c256277515157532d1da04140aac17a296a9
Itélio Lucas <itelyu@gmail.com>
2021-02-10 12:11:53 +0200

    Adds children and adults filter to query

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/IMER1BNumeratorCohortQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/IMER1BNumeratorCohortQueries.java
@@ -20,15 +20,19 @@ public class IMER1BNumeratorCohortQueries {
 
   private TxNewCohortQueries txNewCohortQueries;
 
+  private GenericCohortQueries genericCohortQueries;
+
   private IMER1DenominatorCohortQueries imer1DenominatorCohortQueries;
 
   @Autowired
   public IMER1BNumeratorCohortQueries(
       HivMetadata hivMetadata,
       TxNewCohortQueries txNewCohortQueries,
+      GenericCohortQueries genericCohortQueries,
       IMER1DenominatorCohortQueries imer1DenominatorCohortQueries) {
     this.hivMetadata = hivMetadata;
     this.txNewCohortQueries = txNewCohortQueries;
+    this.genericCohortQueries = genericCohortQueries;
     this.imer1DenominatorCohortQueries = imer1DenominatorCohortQueries;
   }
 
@@ -208,7 +212,7 @@ public class IMER1BNumeratorCohortQueries {
   public CohortDefinition getChildren() {
 
     CompositionCohortDefinition cd = new CompositionCohortDefinition();
-    cd.setName("IMER1B - BreastFeeding");
+    cd.setName("IMER1B - Children");
     cd.addParameter(new Parameter("startDate", "Start Date", Date.class));
     cd.addParameter(new Parameter("endDate", "End Date", Date.class));
     cd.addParameter(new Parameter("location", "Location", Date.class));
@@ -242,7 +246,13 @@ public class IMER1BNumeratorCohortQueries {
         EptsReportUtils.map(
             getE(true), "onOrAfter=${endDate-2m+1d},onOrBefore=${endDate},location=${location}"));
 
-    cd.setCompositionString("A AND NOT B AND NOT C NOT D AND E");
+    cd.addSearch(
+        "CHILDREN",
+        EptsReportUtils.map(
+            genericCohortQueries.getAgeOnPreArtDate(0, 14),
+            "onOrAfter=${endDate-2m+1d},onOrBefore=${endDate-1m},location=${location}"));
+
+    cd.setCompositionString("A AND NOT B AND NOT C NOT D AND E AND CHILDREN");
 
     return cd;
   }
@@ -293,7 +303,13 @@ public class IMER1BNumeratorCohortQueries {
         EptsReportUtils.map(
             getE(true), "onOrAfter=${endDate-2m+1d},onOrBefore=${endDate},location=${location}"));
 
-    cd.setCompositionString("A and NOT B and NOT C NOT D and E");
+    cd.addSearch(
+        "ADULTS",
+        EptsReportUtils.map(
+            genericCohortQueries.getAgeOnPreArtDate(15, 200),
+            "onOrAfter=${endDate-2m+1d},onOrBefore=${endDate-1m},location=${location}"));
+
+    cd.setCompositionString("A and NOT B and NOT C NOT D and E AND ADULTS");
 
     return cd;
   }
