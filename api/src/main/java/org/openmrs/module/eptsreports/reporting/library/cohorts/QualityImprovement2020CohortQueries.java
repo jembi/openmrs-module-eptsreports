68d9b9ba8bbe26dc8313df4a7296561f5a1dadff
ningosi <nicotwendelee@gmail.com>
2021-02-19 21:08:52 +0300

    Bring the alias for the table of A1

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/QualityImprovement2020CohortQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/QualityImprovement2020CohortQueries.java
@@ -5866,8 +5866,8 @@ public class QualityImprovement2020CohortQueries {
     comp.addParameter(new Parameter("revisionEndDate", "revisionEndDate", Date.class));
     comp.addParameter(new Parameter("location", "location", Date.class));
 
-    CohortDefinition queryA =
-        QualityImprovement2020Queries.getMQ15DenA(
+    CohortDefinition queryA1 =
+        QualityImprovement2020Queries.getMQ15DenA1(
             hivMetadata.getAdultoSeguimentoEncounterType().getEncounterTypeId(),
             hivMetadata.getStartDrugs().getConceptId(),
             hivMetadata.getQuarterlyConcept().getConceptId(),
@@ -5902,9 +5902,9 @@ public class QualityImprovement2020CohortQueries {
     CohortDefinition transferOut = commonCohortQueries.getTranferredOutPatients();
 
     comp.addSearch(
-        "A",
+        "A1",
         EptsReportUtils.map(
-            queryA,
+            queryA1,
             "startDate=${revisionEndDate-14m},endDate=${revisionEndDate-11m},location=${location}")); // (A1 OR A3
 
     comp.addSearch("A2", EptsReportUtils.map(queryA2, MAPPING1));
@@ -5930,7 +5930,7 @@ public class QualityImprovement2020CohortQueries {
     comp.addSearch("F", EptsReportUtils.map(transferOut, MAPPING1));
 
     if (den == 1 || den == 2 || den == 3 || den == 4) {
-      comp.setCompositionString("A AND NOT (C OR D OR F)");
+      comp.setCompositionString("(A1 OR A3) AND NOT (C OR D OR F)");
     } else if (den == 5 || den == 7 || den == 9 || den == 11) {
       comp.setCompositionString("(A2 OR A3) AND  NOT (C OR D OR F)");
     } else if (den == 6 || den == 8 || den == 10 || den == 12) {
@@ -6035,8 +6035,8 @@ public class QualityImprovement2020CohortQueries {
     comp.addParameter(new Parameter("revisionEndDate", "revisionEndDate", Date.class));
     comp.addParameter(new Parameter("location", "location", Location.class));
 
-    CohortDefinition queryA =
-        QualityImprovement2020Queries.getMQ15DenA(
+    CohortDefinition queryA1 =
+        QualityImprovement2020Queries.getMQ15DenA1(
             hivMetadata.getAdultoSeguimentoEncounterType().getEncounterTypeId(),
             hivMetadata.getStartDrugs().getConceptId(),
             hivMetadata.getQuarterlyConcept().getConceptId(),
@@ -6116,9 +6116,9 @@ public class QualityImprovement2020CohortQueries {
     CohortDefinition transferOut = commonCohortQueries.getTranferredOutPatients();
 
     comp.addSearch(
-        "A",
+        "A1",
         EptsReportUtils.map(
-            queryA,
+            queryA1,
             "startDate=${revisionEndDate-14m},endDate=${revisionEndDate-11m},location=${location}")); // A1 OR A3
 
     comp.addSearch("A2", EptsReportUtils.map(queryA2, MAPPING1));
@@ -6156,13 +6156,13 @@ public class QualityImprovement2020CohortQueries {
     comp.addSearch("I", EptsReportUtils.map(i, MAPPING1));
 
     if (num == 1) {
-      comp.setCompositionString("A AND NOT (C OR D OR F) AND G2");
+      comp.setCompositionString("(A1 OR A3) AND NOT (C OR D OR F) AND G2");
     } else if (num == 2) {
-      comp.setCompositionString("A AND NOT (C OR D OR F) AND H1");
+      comp.setCompositionString("(A1 OR A3) AND NOT (C OR D OR F) AND H1");
     } else if (num == 3) {
-      comp.setCompositionString("A AND NOT (C OR D OR F) AND H2 AND G2");
+      comp.setCompositionString("(A1 OR A3) AND NOT (C OR D OR F) AND H2 AND G2");
     } else if (num == 4) {
-      comp.setCompositionString("A AND NOT (C OR D OR F) AND G2 AND I");
+      comp.setCompositionString("(A1 OR A3) AND NOT (C OR D OR F) AND G2 AND I");
     } else if (num == 5 || num == 6) {
       comp.setCompositionString("(A2 OR A3) AND NOT (C OR D OR F) AND G2");
     } else if (num == 7 || num == 8) {
