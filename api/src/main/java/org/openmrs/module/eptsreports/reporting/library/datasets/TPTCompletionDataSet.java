21e1fadcd7873de246a77d60caa31fdbb1cee37b
amosh101 <amosh101@outlook.com>
2021-03-11 09:25:53 +0200

    WIP

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/TPTCompletionDataSet.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/TPTCompletionDataSet.java
@@ -1,19 +1,12 @@
 package org.openmrs.module.eptsreports.reporting.library.datasets;
 
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-import org.openmrs.Location;
 import org.openmrs.module.eptsreports.reporting.library.cohorts.TPTCompletionCohortQueries;
 import org.openmrs.module.eptsreports.reporting.library.dimensions.AgeDimensionCohortInterface;
 import org.openmrs.module.eptsreports.reporting.library.dimensions.EptsCommonDimension;
 import org.openmrs.module.eptsreports.reporting.library.indicators.EptsGeneralIndicator;
 import org.openmrs.module.eptsreports.reporting.utils.EptsReportUtils;
-import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
 import org.openmrs.module.reporting.dataset.definition.CohortIndicatorDataSetDefinition;
 import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
-import org.openmrs.module.reporting.evaluation.parameter.Parameter;
-import org.openmrs.module.reporting.indicator.CohortIndicator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -31,10 +24,10 @@ public class TPTCompletionDataSet extends BaseDataSet {
   @Qualifier("commonAgeDimensionCohort")
   private AgeDimensionCohortInterface ageDimensionCohort;
 
-  public DataSetDefinition constructTPTCompletionDataSetDataSet() {
+  public DataSetDefinition constructTPTCompletionDataSet() {
 
     CohortIndicatorDataSetDefinition dataSetDefinition = new CohortIndicatorDataSetDefinition();
-    dataSetDefinition.setName("Quality Improvement DataSet 2020");
+    dataSetDefinition.setName("TPT Completion Cascade Report");
     dataSetDefinition.addParameters(getParameters());
 
     /* add dimensions */
@@ -58,61 +51,10 @@ public class TPTCompletionDataSet extends BaseDataSet {
         EptsReportUtils.map(
             eptsCommonDimension.getPatientAgeBasedOnFirstViralLoadDate(),
             "startDate=${startDate},endDate=${endDate},location=${location}"));
-    
-            // Category 3 Denominator
-    CohortIndicator MQC3D1 =
-        eptsGeneralIndicator.getIndicator(
-            "MQC3D1",
-            EptsReportUtils.map(
-                this.qualityImprovement2020CohortQueries.getMQC3D1(),
-                "startDate=${startDate},endDate=${endDate},location=${location}"));
 
-    addRow(
-        dataSetDefinition,
-        "MQC3D1",
-        "Category 3 Denominator",
-        EptsReportUtils.map(
-            MQC3D1, "startDate=${startDate},endDate=${endDate},location=${location}"),
-        getDisagregateAdultsAndChildrenSColumn());
+    // Disaggregation
 
-    // Category 3 Numerator
-    CohortIndicator MQC3N1 =
-        eptsGeneralIndicator.getIndicator(
-            "MQC3N1",
-            EptsReportUtils.map(
-                this.qualityImprovement2020CohortQueries.getMQC3N1(),
-                "startDate=${startDate},endDate=${endDate},location=${location}"));
-
-    addRow(
-        dataSetDefinition,
-        "MQC3N1",
-        "Category 3 Numerator",
-        EptsReportUtils.map(
-            MQC3N1, "startDate=${startDate},endDate=${endDate},location=${location}"),
-        getDisagregateAdultsAndChildrenSColumn());
-
-       return dataSetDefinition;
-  }
-
-  private List<ColumnParameters> getDisagregateAdultsAndChildrenSColumn() {
-    ColumnParameters ADULTOS = new ColumnParameters("ADULTOS", "Adultos", "age=15+", "ADULTOS");
-    ColumnParameters CRIANCAS = new ColumnParameters("CRIANCAS", "Criancas", "age=<15", "CRIANCAS");
-    return Arrays.asList(ADULTOS, CRIANCAS);
-  }
-
-  @Override
-  public List<Parameter> getParameters() {
-    return Arrays.asList(
-        new Parameter("startDate", "Data Inicial Inclusão", Date.class),
-        new Parameter("endDate", "Data Final Inclusão", Date.class),
-        new Parameter("revisionEndDate", "Data Final Revisão", Date.class),
-        new Parameter("location", "Unidade Sanitária", Location.class));
-  }
-
-  private CohortIndicator customCohortIndicator(CohortDefinition cd, String mapping) {
-    CohortIndicator cohortIndicator =
-        eptsGeneralIndicator.getIndicator(cd.getName(), EptsReportUtils.map(cd, mapping));
-    cohortIndicator.addParameter(new Parameter("revisionEndDate", "Revision Date", Date.class));
-    return cohortIndicator;
+    // Details
+    return dataSetDefinition;
   }
 }
--- api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupTPTCompletionReport.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupTPTCompletionReport.java
@@ -2,16 +2,13 @@ package org.openmrs.module.eptsreports.reporting.reports;
 
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 import java.util.Properties;
-import org.openmrs.Location;
 import org.openmrs.module.eptsreports.reporting.library.cohorts.GenericCohortQueries;
 import org.openmrs.module.eptsreports.reporting.library.datasets.TPTCompletionDataSet;
 import org.openmrs.module.eptsreports.reporting.reports.manager.EptsDataExportManager;
 import org.openmrs.module.eptsreports.reporting.utils.EptsReportUtils;
 import org.openmrs.module.reporting.evaluation.parameter.Mapped;
-import org.openmrs.module.reporting.evaluation.parameter.Parameter;
 import org.openmrs.module.reporting.report.ReportDesign;
 import org.openmrs.module.reporting.report.definition.ReportDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -57,8 +54,7 @@ public class SetupTPTCompletionReport extends EptsDataExportManager {
     reportDefinition.setDescription(getDescription());
     reportDefinition.setParameters(getParameters());
     reportDefinition.addDataSetDefinition(
-        "ALL",
-        Mapped.mapStraightThrough(tPTCompletionDataSet.constructTPTCompletionDataSet()));
+        "ALL", Mapped.mapStraightThrough(tPTCompletionDataSet.constructTPTCompletionDataSet()));
 
     // add a base cohort here to help in calculations running
     reportDefinition.setBaseCohortDefinition(
@@ -88,12 +84,5 @@ public class SetupTPTCompletionReport extends EptsDataExportManager {
     }
 
     return Arrays.asList(reportDesign);
-  }
-
-  @Override
-  public List<Parameter> getParameters() {
-    return Arrays.asList(
-        new Parameter("startDate", "Data Inicial Inclusão", Date.class),
-        new Parameter("endDate", "Data Final Inclusão", Date.class);
   }
 }
