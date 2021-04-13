9c37af5e959daf8d8d445badd2da818b715c96b1
cfaife <clerio.faife@gmail.com>
2021-04-13 13:01:17 +0200

    MEPTS-809: fixed the template issues

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupIntensiveMonitoringReport.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupIntensiveMonitoringReport.java
@@ -1,5 +1,10 @@
 package org.openmrs.module.eptsreports.reporting.reports;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Properties;
 import org.openmrs.Location;
 import org.openmrs.module.eptsreports.reporting.library.cohorts.GenericCohortQueries;
 import org.openmrs.module.eptsreports.reporting.library.datasets.QualityImprovement2020DataSet;
@@ -12,92 +17,85 @@ import org.openmrs.module.reporting.report.definition.ReportDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
-import java.util.Properties;
-
 @Component
 public class SetupIntensiveMonitoringReport extends EptsDataExportManager {
 
-    @Autowired
-    protected GenericCohortQueries genericCohortQueries;
+  @Autowired protected GenericCohortQueries genericCohortQueries;
 
-    @Autowired private QualityImprovement2020DataSet initQltyImpDataSet;
+  @Autowired private QualityImprovement2020DataSet initQltyImpDataSet;
 
-    @Override
-    public String getUuid() {
-        return "e248b75a-9b85-11eb-a09b-338d8a9a6376";
+  @Override
+  public String getUuid() {
+    return "e248b75a-9b85-11eb-a09b-338d8a9a6376";
+  }
+
+  @Override
+  public String getExcelDesignUuid() {
+    return "91b97104-9c29-11eb-b128-2306b37b08da";
+  }
+
+  @Override
+  public String getName() {
+    return "Intensive Monitoring";
+  }
+
+  @Override
+  public String getDescription() {
+    return "Relatórios com os Indicadores Monitoria de Intensiva";
+  }
+
+  @Override
+  public String getVersion() {
+    return "1.0-SNAPSHOT";
+  }
+
+  @Override
+  public ReportDefinition constructReportDefinition() {
+    ReportDefinition reportDefinition = new ReportDefinition();
+    reportDefinition.setUuid(getUuid());
+    reportDefinition.setName(getName());
+    reportDefinition.setDescription(getDescription());
+    reportDefinition.setParameters(getParameters());
+    reportDefinition.addDataSetDefinition(
+        "ALL",
+        Mapped.mapStraightThrough(initQltyImpDataSet.constructQualityImprovement2020DataSet()));
+
+    // add a base cohort here to help in calculations running
+    reportDefinition.setBaseCohortDefinition(
+        EptsReportUtils.map(
+            genericCohortQueries.getBaseCohort(),
+            "endDate=${revisionEndDate},location=${location}"));
+
+    return reportDefinition;
+  }
+
+  @Override
+  public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
+    ReportDesign reportDesign = null;
+    try {
+      reportDesign =
+          createXlsReportDesign(
+              reportDefinition,
+              "Monitoria_Intensiva_HIV_v1.xls",
+              "Template Ficha Relatório Monitoria Intensiva HIV",
+              getExcelDesignUuid(),
+              null);
+      Properties props = new Properties();
+      props.put("sortWeight", "5000");
+      reportDesign.setProperties(props);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
 
-    @Override
-    public String getName() {
-        return "Intensive Monitoring";
-    }
+    return Arrays.asList(reportDesign);
+  }
 
-    @Override
-    public String getDescription() {
-        return "Relatórios com os Indicadores Monitoria de Intensiva";
-    }
-
-    @Override
-    public String getVersion() {
-        return "1.0-SNAPSHOT";
-    }
-
-    @Override
-    public String getExcelDesignUuid() {
-        return "dbaa078c-9b85-11eb-8d48-ff12ffdfe6f6";
-    }
-
-    @Override
-    public ReportDefinition constructReportDefinition() {
-        ReportDefinition reportDefinition = new ReportDefinition();
-        reportDefinition.setUuid(getUuid());
-        reportDefinition.setName(getName());
-        reportDefinition.setDescription(getDescription());
-        reportDefinition.setParameters(getParameters());
-        reportDefinition.addDataSetDefinition(
-                "ALL",
-                Mapped.mapStraightThrough(initQltyImpDataSet.constructQualityImprovement2020DataSet()));
-
-        // add a base cohort here to help in calculations running
-        reportDefinition.setBaseCohortDefinition(
-                EptsReportUtils.map(
-                        genericCohortQueries.getBaseCohort(),
-                        "endDate=${revisionEndDate},location=${location}"));
-
-        return reportDefinition;
-    }
-
-    @Override
-    public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
-        ReportDesign reportDesign = null;
-        try {
-            reportDesign =
-                    createXlsReportDesign(
-                            reportDefinition,
-                            "Template_Ficha_Relatório_Monitoria_Intensiva_HIV_v1.xlsx",
-                            "Template Ficha Relatório Monitoria Intensiva HIV",
-                            getExcelDesignUuid(),
-                            null);
-            Properties props = new Properties();
-            props.put("sortWeight", "5000");
-            reportDesign.setProperties(props);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        return Arrays.asList(reportDesign);
-    }
-
-    @Override
-    public List<Parameter> getParameters() {
-        return Arrays.asList(
-                new Parameter("startDate", "Data Inicial Inclusão", Date.class),
-                new Parameter("endDate", "Data Final Inclusão", Date.class),
-                new Parameter("revisionEndDate", "Data Final Revisão", Date.class),
-                new Parameter("location", "Unidade Sanitária", Location.class));
-    }
+  @Override
+  public List<Parameter> getParameters() {
+    return Arrays.asList(
+        new Parameter("startDate", "Data Inicial Inclusão", Date.class),
+        new Parameter("endDate", "Data Final Inclusão", Date.class),
+        new Parameter("revisionEndDate", "Data Final Revisão", Date.class),
+        new Parameter("location", "Unidade Sanitária", Location.class));
+  }
 }
