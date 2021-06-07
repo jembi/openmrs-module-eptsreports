9fe84616173eb7c181cba0fd659acc9bb8bfc107
francisco12 <f.rupansana@gmail.com>
2021-06-07 15:58:35 +0200

    update cat 13 part-1

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/IntensiveMonitoringCohortQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/IntensiveMonitoringCohortQueries.java
@@ -98,48 +98,8 @@ public class IntensiveMonitoringCohortQueries {
   }
 
   /**
-   * Get CAT 13 Part-1 DENOMINADOR Monitoria Intensiva MQHIV 2021 for the selected location and
-   * reporting period Section (endDateRevision)
-   *
-   * @return @{@link org.openmrs.module.reporting.cohort.definition.CohortDefinition}
-   */
-  public CohortDefinition getMICat13Part1DEN(Integer level) {
-    CompositionCohortDefinition cd = new CompositionCohortDefinition();
-    cd.setName("MI 13 Part-1 Denominator");
-    cd.addParameter(new Parameter("location", "location", Location.class));
-    cd.addParameter(new Parameter("revisionEndDate", "revisionEndDate", Date.class));
-    cd.addSearch(
-        "MI13DEN",
-        EptsReportUtils.map(
-            qualityImprovement2020CohortQueries.getMQ13(true, level),
-            "startDate=${revisionEndDate-2m+1d},endDate=${revisionEndDate-1m},location=${location}"));
-    cd.setCompositionString("MI13DEN");
-    return cd;
-  }
-
-  /**
-   * Get CAT 13 Part-1 NUMERADOR Monitoria Intensiva MQHIV 2021 for the selected location and
-   * reporting period Section (endDateRevision)
-   *
-   * @return @{@link org.openmrs.module.reporting.cohort.definition.CohortDefinition}
-   */
-  public CohortDefinition getMICat13Part1NUM(Integer level) {
-    CompositionCohortDefinition cd = new CompositionCohortDefinition();
-    cd.setName("MI 13 Part-1 Numerador");
-    cd.addParameter(new Parameter("location", "location", Location.class));
-    cd.addParameter(new Parameter("revisionEndDate", "revisionEndDate", Date.class));
-    cd.addSearch(
-        "MI13NUM",
-        EptsReportUtils.map(
-            qualityImprovement2020CohortQueries.getMQ13(false, level),
-            "startDate=${revisionEndDate-2m+1d},endDate=${revisionEndDate-1m},location=${location}"));
-    cd.setCompositionString("MI13NUM");
-    return cd;
-  }
-
-  /**
    * Get CAT 13.1, 13.6, 13.7, 13.8 Monitoria Intensiva MQHIV 2021 for the selected location and
-   * reporting period Section 7.1 (endDateRevision)
+   * reporting period Section (endDateRevision)
    *
    * @return @{@link org.openmrs.module.reporting.cohort.definition.CohortDefinition}
    */
