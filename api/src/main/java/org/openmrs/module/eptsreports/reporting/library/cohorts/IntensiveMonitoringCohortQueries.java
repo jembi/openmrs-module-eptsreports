8c0f49367f4b8eef9d308422a1152befb14265bc
helderjosue <helderjosuemata@gmail.com>
2021-06-18 16:35:48 +0200

    changing mapping2

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/IntensiveMonitoringCohortQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/IntensiveMonitoringCohortQueries.java
@@ -33,7 +33,7 @@ public class IntensiveMonitoringCohortQueries {
   private TbMetadata tbMetadata;
 
   private final String MAPPING2 =
-      "startDate=${revisionEndDate-5m+1d},endDate=${revisionEndDate-4m},revisionEndDate=${revisionEndDate},location=${location}";
+      "startDate=${revisionEndDate-5m+1d},endDate=${revisionEndDate-4m},location=${location}";
 
   private final String MAPPING3 =
       "startDate=${revisionEndDate-5m+1d},endDate=${revisionEndDate-4m},revisionEndDate=${revisionEndDate},location=${location}";
