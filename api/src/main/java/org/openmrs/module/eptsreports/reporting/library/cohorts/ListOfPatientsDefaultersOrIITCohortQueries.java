719e253520d4faeb3ad4a30c06341382e58cde9f
francisco12 <f.rupansana@gmail.com>
2021-08-17 10:24:09 +0200

    first commit

---
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/ListOfPatientsDefaultersOrIITCohortQueries.java
@@ -0,0 +1,8 @@
+package org.openmrs.module.eptsreports.reporting.library.cohorts;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ListOfPatientsDefaultersOrIITCohortQueries {
+
+}
