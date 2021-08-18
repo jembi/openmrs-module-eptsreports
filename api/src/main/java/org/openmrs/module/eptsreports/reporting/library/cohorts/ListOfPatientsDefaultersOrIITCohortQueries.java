4a133fd98cb294be8acfe8a7d7458c6394649753
francisco12 <f.rupansana@gmail.com>
2021-08-18 17:14:13 +0200

    add e2.1 to e2.4

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/ListOfPatientsDefaultersOrIITCohortQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/ListOfPatientsDefaultersOrIITCohortQueries.java
@@ -5,9 +5,8 @@ import java.util.HashMap;
 import java.util.Map;
 import org.apache.commons.text.StringSubstitutor;
 import org.openmrs.Location;
-import org.openmrs.Program;
-import org.openmrs.ProgramWorkflowState;
 import org.openmrs.module.eptsreports.metadata.HivMetadata;
+import org.openmrs.module.eptsreports.reporting.library.queries.TXCurrQueries;
 import org.openmrs.module.eptsreports.reporting.utils.EptsReportUtils;
 import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
 import org.openmrs.module.reporting.cohort.definition.CompositionCohortDefinition;
@@ -27,7 +26,7 @@ public class ListOfPatientsDefaultersOrIITCohortQueries {
 
   @Autowired private HivMetadata hivMetadata;
 
-  private final String MAPPING = "endDate=${endDate},location=${location}";
+  private final String MAPPING = "location=${location}";
 
   private final String MAPPING2 = "onOrBefore=${endDate},location=${location}";
 
@@ -102,18 +101,18 @@ public class ListOfPatientsDefaultersOrIITCohortQueries {
     cd.addParameter(new Parameter("endDate", "EndDate", Date.class));
     cd.addParameter(new Parameter("location", "Location", Location.class));
 
-    CohortDefinition e21 = getPatientsInArtCareWhoDied();
-    CohortDefinition e22 = txCurrCohortQueries.getDeadPatientsInDemographiscByReportingEndDate();
+    CohortDefinition e21 = getE21();
+    CohortDefinition e22 = getE22();
     CohortDefinition e23 =
-        txCurrCohortQueries.getPatientDeathRegisteredInLastHomeVisitCardByReportingEndDate();
+            getPatientDeathRegisteredInLastHomeVisitCardByReportingEndDate();
     CohortDefinition e24 =
-        txCurrCohortQueries.getDeadPatientsInFichaResumeAndClinicaOfMasterCardByReportEndDate();
+            getDeadPatientsInFichaResumeAndClinicaOfMasterCardByReportEndDate();
     CohortDefinition e25 = getE25();
 
     cd.addSearch("e21", EptsReportUtils.map(e21, MAPPING));
-    cd.addSearch("e22", EptsReportUtils.map(e22, MAPPING3));
-    cd.addSearch("e23", EptsReportUtils.map(e23, MAPPING2));
-    cd.addSearch("e24", EptsReportUtils.map(e24, MAPPING2));
+    cd.addSearch("e22", EptsReportUtils.map(e22, MAPPING));
+    cd.addSearch("e23", EptsReportUtils.map(e23, MAPPING));
+    cd.addSearch("e24", EptsReportUtils.map(e24, MAPPING));
     cd.addSearch("e25", EptsReportUtils.map(e25, MAPPING));
 
     cd.setCompositionString("(e21 OR e22 OR e23 OR e24) AND e25");
@@ -121,12 +120,237 @@ public class ListOfPatientsDefaultersOrIITCohortQueries {
     return cd;
   }
 
-  public CohortDefinition getPatientsInArtCareWhoDied() {
-    Program hivCareProgram = hivMetadata.getARTProgram();
-    ProgramWorkflowState dead = hivMetadata.getArtDeadWorkflowState();
-    return genericCohortQueries.getPatientsBasedOnPatientStatesBeforeDate(
-        hivCareProgram.getProgramId(), dead.getProgramWorkflowStateId());
+  /**
+   * <b>2.4</b> - All deaths registered in Patient Program State by reporting end date *
+   * Patient_program.program_id =2 = SERVICO TARV-TRATAMENTO and Patient_State.state = 10 (Died) *
+   * Patient_State.start_date <= Report Generation Date Patient_state.end_date is null
+   *
+   * @return
+   */
+  public SqlCohortDefinition getE21() {
+
+    SqlCohortDefinition sqlCohortDefinition = new SqlCohortDefinition();
+    sqlCohortDefinition.setName(" All patients that started ART during inclusion period ");
+    sqlCohortDefinition.addParameter(new Parameter("endDate", "EndDate", Date.class));
+    sqlCohortDefinition.addParameter(new Parameter("location", "Location", Location.class));
+
+    Map<String, Integer> map = new HashMap<>();
+    map.put("2", hivMetadata.getARTProgram().getProgramId());
+    map.put("10", hivMetadata.getArtDeadWorkflowState().getProgramWorkflowStateId());
+
+    String query =
+        "SELECT p.patient_id FROM patient p    "
+            + "    INNER JOIN patient_program pg ON pg.patient_id = p.patient_id "
+            + "    INNER JOIN patient_state ps ON ps.patient_program_id = pg.patient_program_id "
+            + " WHERE  pg.program_id = ${2}  "
+            + " AND ps.state = ${10} "
+            + " AND ps.start_date <= CURRENT_DATE() "
+            + " AND ps.end_date IS NULL "
+            + " AND pg.location_id = :location";
+
+    StringSubstitutor stringSubstitutor = new StringSubstitutor(map);
+
+    sqlCohortDefinition.setQuery(stringSubstitutor.replace(query));
+
+    return sqlCohortDefinition;
   }
+
+  /**
+   * <b>2.2</b> - All deaths registered in Patient Demographics by reporting end date
+   *    * Person.Dead=1 and death_date <= Report Generation Date
+   *
+   * @return
+   */
+  public SqlCohortDefinition getE22() {
+
+    SqlCohortDefinition sqlCohortDefinition = new SqlCohortDefinition();
+    sqlCohortDefinition.setName(" All patients that started ART during inclusion period ");
+    sqlCohortDefinition.addParameter(new Parameter("endDate", "EndDate", Date.class));
+    sqlCohortDefinition.addParameter(new Parameter("location", "Location", Location.class));
+
+    Map<String, Integer> map = new HashMap<>();
+
+    String query =
+            "SELECT p.person_id   "
+                    + "                FROM person p  "
+                    + "                INNER JOIN patient_program pg ON p.person_id=pg.patient_id "
+                    + "                INNER JOIN patient_state ps ON pg.patient_program_id=ps.patient_program_id "
+                    + "                WHERE p.dead=1  "
+                    + "                AND p.death_date <= CURRENT_DATE()  "
+                    + "                AND p.voided=0";
+
+    StringSubstitutor stringSubstitutor = new StringSubstitutor(map);
+
+    sqlCohortDefinition.setQuery(stringSubstitutor.replace(query));
+
+    return sqlCohortDefinition;
+  }
+
+  /**
+   * <p><b>2.3</b> - All deaths registered in Last Home Visit Card by reporting end date Last Home
+   * Visit Card (Encounter Type 21, 36, 37) Reason of Not Finding (Concept ID 2031 or 23944 or
+   * 23945) = Died (Concept Id 1366) Last Encounter_datetime <= Report Generation Date
+   *
+   * @param buscaActivaEncounterType
+   * @param visitaApoioReintegracaoParteAEncounterType
+   * @param visitaApoioReintegracaoParteBEncounterType
+   * @param reasonPatientNotFound
+   * @param reasonPatientNotFoundByActivist2ndVisit
+   * @param reasonPatientNotFoundByActivist3rdVisit
+   * @param patientIsDead
+   * @return
+   */
+  public static String getPatientDeathRegisteredInLastHomeVisitCardByReportingEndDate(
+          int buscaActivaEncounterType,
+          int visitaApoioReintegracaoParteAEncounterType,
+          int visitaApoioReintegracaoParteBEncounterType,
+          int reasonPatientNotFound,
+          int reasonPatientNotFoundByActivist2ndVisit,
+          int reasonPatientNotFoundByActivist3rdVisit,
+          int patientIsDead) {
+    String query =
+            "  SELECT  max_date.patient_id FROM  "
+                    + "    (SELECT  "
+                    + "      p.patient_id,  "
+                    + "      MAX(e.encounter_datetime) last   "
+                    + "    FROM patient p "
+                    + "      INNER  JOIN encounter e ON e.patient_id=p.patient_id "
+                    + "     WHERE  "
+                    + "      e.encounter_datetime <= CURRENT_DATE() "
+                    + "      AND e.location_id = :location "
+                    + "      AND e.encounter_type  in( ${buscaActiva},${visitaApoioReintegracaoParteA},${visitaApoioReintegracaoParteB})  "
+                    + "      AND e.voided=0 "
+                    + "      AND p.voided = 0 "
+                    + "    GROUP BY  p.patient_id  ) max_date "
+                    + "    INNER  JOIN encounter ee "
+                    + "            ON ee.patient_id = max_date.patient_id "
+                    + "    INNER  JOIN obs o ON ee.encounter_id = o.encounter_id  "
+                    + "        WHERE  "
+                    + "        ( "
+                    + "            (o.concept_id = ${reasonPatientNotFound} AND o.value_coded = ${patientIsDead}) OR "
+                    + "            (o.concept_id = ${reasonPatientNotFoundByActivist2ndVisit} AND o.value_coded = ${patientIsDead}) OR "
+                    + "            (o.concept_id = ${reasonPatientNotFoundByActivist3rdVisit} AND o.value_coded = ${patientIsDead} ) "
+                    + "        )  "
+                    + "    AND o.voided=0 "
+                    + "    AND ee.voided = 0 "
+                    + "    GROUP BY  max_date.patient_id";
+
+    Map<String, Integer> map = new HashMap<>();
+    map.put("buscaActiva", buscaActivaEncounterType);
+    map.put("visitaApoioReintegracaoParteA", visitaApoioReintegracaoParteAEncounterType);
+    map.put("visitaApoioReintegracaoParteB", visitaApoioReintegracaoParteBEncounterType);
+    map.put("reasonPatientNotFound", reasonPatientNotFound);
+    map.put("reasonPatientNotFoundByActivist2ndVisit", reasonPatientNotFoundByActivist2ndVisit);
+    map.put("reasonPatientNotFoundByActivist3rdVisit", reasonPatientNotFoundByActivist3rdVisit);
+    map.put("patientIsDead", patientIsDead);
+
+    StringSubstitutor sub = new StringSubstitutor(map);
+    return sub.replace(query);
+  }
+
+  public CohortDefinition getPatientDeathRegisteredInLastHomeVisitCardByReportingEndDate() {
+    SqlCohortDefinition definition = new SqlCohortDefinition();
+    definition.setName("patientDeathRegisteredInLastHomeVisitCardByReportingEndDate");
+
+    definition.setQuery(
+            getPatientDeathRegisteredInLastHomeVisitCardByReportingEndDate(
+                    hivMetadata.getBuscaActivaEncounterType().getEncounterTypeId(),
+                    hivMetadata.getVisitaApoioReintegracaoParteAEncounterType().getEncounterTypeId(),
+                    hivMetadata.getVisitaApoioReintegracaoParteBEncounterType().getEncounterTypeId(),
+                    hivMetadata.getReasonPatientNotFound().getConceptId(),
+                    hivMetadata.getReasonPatientNotFoundByActivist2ndVisitConcept().getConceptId(),
+                    hivMetadata.getReasonPatientNotFoundByActivist3rdVisitConcept().getConceptId(),
+                    hivMetadata.getPatientHasDiedConcept().getConceptId()));
+
+    definition.addParameter(new Parameter("onOrBefore", "onOrBefore", Date.class));
+    definition.addParameter(new Parameter("location", "location", Location.class));
+
+    return definition;
+  }
+
+  /**
+   <p><b>2.4</b> - All deaths registered in Ficha Resumo and Ficha Clinica of Master Card by
+   * reporting end date Encounter Type ID= 6 Estado de Permanencia (Concept Id 6273) = Dead (Concept
+   * ID 1366) Encounter_datetime <= Report Generation Date OR Encounter Type ID= 53 Estado de
+   * Permanencia (Concept Id 6272) = Dead (Concept ID 1366) obs_datetime <= Report Generation Date
+   *
+   * @param adultoSeguimentoEncounterType
+   * @param masterCardEncounterType
+   * @param stateOfStayPriorArtPatientConcept
+   * @param stateOfStayOfArtPatient
+   * @param patientHasDiedConcept
+   * @return
+   */
+  public static String getDeadPatientsInFichaResumeAndClinicaOfMasterCardByReportEndDate(
+          int adultoSeguimentoEncounterType,
+          int masterCardEncounterType,
+          int stateOfStayPriorArtPatientConcept,
+          int stateOfStayOfArtPatient,
+          int patientHasDiedConcept) {
+
+    Map<String, Integer> map = new HashMap<>();
+    map.put("adultoSeguimentoEncounterType", adultoSeguimentoEncounterType);
+    map.put("masterCardEncounterType", masterCardEncounterType);
+    map.put("stateOfStayPriorArtPatientConcept", stateOfStayPriorArtPatientConcept);
+    map.put("stateOfStayOfArtPatient", stateOfStayOfArtPatient);
+    map.put("patientHasDiedConcept", patientHasDiedConcept);
+
+    String query =
+            "SELECT  p.patient_id  "
+                    + "FROM patient p  "
+                    + "    INNER JOIN encounter e  "
+                    + "        ON e.patient_id=p.patient_id  "
+                    + "    INNER JOIN obs o  "
+                    + "        ON o.encounter_id=e.encounter_id  "
+                    + "WHERE e.encounter_type = ${adultoSeguimentoEncounterType} "
+                    + "    AND e.encounter_datetime <= CURRENT_DATE() "
+                    + "    AND o.concept_id = ${stateOfStayOfArtPatient} "
+                    + "    AND o.value_coded=${patientHasDiedConcept}  "
+                    + "    AND e.location_id = :location  "
+                    + "    AND p.voided=0   "
+                    + "    AND e.voided=0  "
+                    + "    AND o.voided=0  "
+                    + "GROUP BY p.patient_id "
+                    + "UNION "
+                    + "SELECT  p.patient_id  "
+                    + "FROM patient p  "
+                    + "    INNER JOIN encounter e  "
+                    + "        ON e.patient_id=p.patient_id  "
+                    + "    INNER JOIN obs o  "
+                    + "        ON o.encounter_id=e.encounter_id  "
+                    + "WHERE e.encounter_type = ${masterCardEncounterType}  "
+                    + "    AND o.obs_datetime <= CURRENT_DATE() "
+                    + "    AND o.concept_id = ${stateOfStayPriorArtPatientConcept} "
+                    + "    AND o.value_coded=${patientHasDiedConcept}  "
+                    + "    AND e.location_id = :location  "
+                    + "    AND p.voided=0   "
+                    + "    AND e.voided=0  "
+                    + "    AND o.voided=0  "
+                    + "GROUP BY p.patient_id ";
+
+    StringSubstitutor stringSubstitutor = new StringSubstitutor(map);
+
+    return stringSubstitutor.replace(query);
+  }
+
+  public CohortDefinition getDeadPatientsInFichaResumeAndClinicaOfMasterCardByReportEndDate() {
+    SqlCohortDefinition definition = new SqlCohortDefinition();
+    definition.setName("deadPatientsInFichaResumeAndClinicaOfMasterCardByReportEndDate");
+
+    definition.setQuery(
+            getDeadPatientsInFichaResumeAndClinicaOfMasterCardByReportEndDate(
+                    hivMetadata.getAdultoSeguimentoEncounterType().getEncounterTypeId(),
+                    hivMetadata.getMasterCardEncounterType().getEncounterTypeId(),
+                    hivMetadata.getStateOfStayPriorArtPatientConcept().getConceptId(),
+                    hivMetadata.getStateOfStayOfArtPatient().getConceptId(),
+                    hivMetadata.getPatientHasDiedConcept().getConceptId()));
+
+    definition.addParameter(new Parameter("onOrBefore", "onOrBefore", Date.class));
+    definition.addParameter(new Parameter("location", "location", Location.class));
+
+    return definition;
+  }
+
 
   /**
    * <b>2.4</b> - All deaths registered in Ficha Resumo and Ficha Clinica of Master Card by
@@ -439,7 +663,7 @@ public class ListOfPatientsDefaultersOrIITCohortQueries {
         hivMetadata
             .getTransferredOutToAnotherHealthFacilityWorkflowState()
             .getProgramWorkflowStateId());
-            map.put("9", hivMetadata.getPediatriaSeguimentoEncounterType().getEncounterTypeId());
+    map.put("9", hivMetadata.getPediatriaSeguimentoEncounterType().getEncounterTypeId());
     map.put("18", hivMetadata.getARVPharmaciaEncounterType().getEncounterTypeId());
     map.put("52", hivMetadata.getMasterCardDrugPickupEncounterType().getEncounterTypeId());
     map.put("53", hivMetadata.getMasterCardEncounterType().getEncounterTypeId());
@@ -556,6 +780,4 @@ public class ListOfPatientsDefaultersOrIITCohortQueries {
 
     return sqlCohortDefinition;
   }
-
-
 }
