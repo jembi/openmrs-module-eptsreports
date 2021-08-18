73b219c24972e2fa918c395c3d5759f3cd19e878
francisco12 <f.rupansana@gmail.com>
2021-08-18 09:10:14 +0200

    add new query

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/ListOfPatientsDefaultersOrIITCohortQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/ListOfPatientsDefaultersOrIITCohortQueries.java
@@ -1,6 +1,9 @@
 package org.openmrs.module.eptsreports.reporting.library.cohorts;
 
 import org.openmrs.Location;
+import org.openmrs.Program;
+import org.openmrs.ProgramWorkflowState;
+import org.openmrs.module.eptsreports.metadata.HivMetadata;
 import org.openmrs.module.eptsreports.reporting.utils.EptsReportUtils;
 import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
 import org.openmrs.module.reporting.cohort.definition.CompositionCohortDefinition;
@@ -15,13 +18,39 @@ public class ListOfPatientsDefaultersOrIITCohortQueries {
     @Autowired
     private TxCurrCohortQueries txCurrCohortQueries;
 
-    public CohortDefinition getE() {
+    @Autowired private CommonCohortQueries commonCohortQueries;
+
+    @Autowired private GenericCohortQueries genericCohortQueries;
+
+    @Autowired private HivMetadata hivMetadata;
+
+    public CohortDefinition getE1() {
         CompositionCohortDefinition cd = new CompositionCohortDefinition();
         cd.addParameter(new Parameter("startDate", "StartDate", Date.class));
         cd.addParameter(new Parameter("endDate", "EndDate", Date.class));
         cd.addParameter(new Parameter("revisionEndDate", "revisionEndDate", Date.class));
         cd.addParameter(new Parameter("location", "Location", Location.class));
 
+        CohortDefinition e12 = commonCohortQueries.getTranferredOutPatients();
+
+
+
+        cd.addSearch("A", EptsReportUtils.map(e12, ""));
+
+
+        cd.setCompositionString(
+                "(A)");
+
+        return cd;
+    }
+    public CohortDefinition getE2() {
+        CompositionCohortDefinition cd = new CompositionCohortDefinition();
+        cd.addParameter(new Parameter("startDate", "StartDate", Date.class));
+        cd.addParameter(new Parameter("endDate", "EndDate", Date.class));
+        cd.addParameter(new Parameter("revisionEndDate", "revisionEndDate", Date.class));
+        cd.addParameter(new Parameter("location", "Location", Location.class));
+
+        CohortDefinition e21 = getPatientsInArtCareWhoDied();
         CohortDefinition e22 = txCurrCohortQueries.getDeadPatientsInDemographiscByReportingEndDate();
         CohortDefinition e23 = txCurrCohortQueries.getPatientDeathRegisteredInLastHomeVisitCardByReportingEndDate();
         CohortDefinition e24 = txCurrCohortQueries.getDeadPatientsInFichaResumeAndClinicaOfMasterCardByReportEndDate();
@@ -30,11 +59,19 @@ public class ListOfPatientsDefaultersOrIITCohortQueries {
         cd.addSearch("A", EptsReportUtils.map(e22, ""));
         cd.addSearch("B", EptsReportUtils.map(e23, ""));
         cd.addSearch("C", EptsReportUtils.map(e24, ""));
+        cd.addSearch("D", EptsReportUtils.map(e21, ""));
 
 
         cd.setCompositionString(
                 "(A OR B)");
 
         return cd;
+    }
+
+    public CohortDefinition getPatientsInArtCareWhoDied() {
+        Program hivCareProgram = hivMetadata.getARTProgram();
+        ProgramWorkflowState dead = hivMetadata.getArtDeadWorkflowState();
+        return genericCohortQueries.getPatientsBasedOnPatientStatesBeforeDate(
+                hivCareProgram.getProgramId(), dead.getProgramWorkflowStateId());
     }
 }
