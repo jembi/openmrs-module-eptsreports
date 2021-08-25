dc704c3fcdbd499297b408ab4a7c07edcf3f2e1f
elmuchave <muchave03@gmail.com>
2021-08-25 15:08:21 +0200

    MEPTS-908 Setup and Dataset creation

---
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/TotalOfPatientsEligibleForVLDataSet.java
@@ -0,0 +1,16 @@
+package org.openmrs.module.eptsreports.reporting.library.datasets;
+
+import org.openmrs.module.reporting.dataset.definition.CohortIndicatorDataSetDefinition;
+import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
+
+public class TotalOfPatientsEligibleForVLDataSet extends BaseDataSet {
+
+  public DataSetDefinition constructDataSet() {
+
+    CohortIndicatorDataSetDefinition dataSetDefinition = new CohortIndicatorDataSetDefinition();
+    dataSetDefinition.setName("Total of patients eligible for VL report");
+    dataSetDefinition.addParameters(getParameters());
+
+    return dataSetDefinition;
+  }
+}
