85ccad46d949eea3a6a8c0aa92f157424ffd5bbe
francisco12 <f.rupansana@gmail.com>
2021-09-08 12:23:31 +0200

    Change the annotation

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupMERSemiAnnualReport.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupMERSemiAnnualReport.java
@@ -13,8 +13,9 @@ import org.openmrs.module.reporting.evaluation.parameter.ParameterizableUtil;
 import org.openmrs.module.reporting.report.ReportDesign;
 import org.openmrs.module.reporting.report.definition.ReportDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-@Deprecated
+@Component
 public class SetupMERSemiAnnualReport extends EptsDataExportManager {
 
   @Autowired private TxMlDataset txMlDataset;
