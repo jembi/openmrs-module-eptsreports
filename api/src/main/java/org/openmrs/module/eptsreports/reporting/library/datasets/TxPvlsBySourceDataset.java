551ed0ae5b90a23b22a94aabb701f976c3702ef2
ningosi <nicotwendelee@gmail.com>
2021-11-29 18:50:08 +0300

    Update the datasets with the dimesions and reporting zeros to the second sheet on the target section as per the specifications

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/TxPvlsBySourceDataset.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/TxPvlsBySourceDataset.java
@@ -2,15 +2,23 @@ package org.openmrs.module.eptsreports.reporting.library.datasets;
 
 import org.openmrs.module.eptsreports.reporting.library.cohorts.TxPvlsBySourceClinicalOrFichaResumoCohortQueries;
 import org.openmrs.module.eptsreports.reporting.library.cohorts.TxPvlsBySourceLabOrFsrCohortQueries;
+import org.openmrs.module.eptsreports.reporting.library.dimensions.AgeDimensionCohortInterface;
+import org.openmrs.module.eptsreports.reporting.library.dimensions.EptsCommonDimension;
+import org.openmrs.module.eptsreports.reporting.library.indicators.EptsGeneralIndicator;
+import org.openmrs.module.eptsreports.reporting.utils.EptsReportUtils;
 import org.openmrs.module.reporting.dataset.definition.CohortIndicatorDataSetDefinition;
 import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TxPvlsBySourceDataset extends BaseDataSet {
 
   private TxPvlsBySourceLabOrFsrCohortQueries txPvlsBySourceLabOrFsrCohortQueries;
+  private EptsCommonDimension eptsCommonDimension;
+  private EptsGeneralIndicator eptsGeneralIndicator;
+  private AgeDimensionCohortInterface ageDimensionCohortInterface;
 
   private TxPvlsBySourceClinicalOrFichaResumoCohortQueries
       txPvlsBySourceClinicalOrFichaResumoCohortQueries;
@@ -19,29 +27,80 @@ public class TxPvlsBySourceDataset extends BaseDataSet {
   public TxPvlsBySourceDataset(
       TxPvlsBySourceLabOrFsrCohortQueries txPvlsBySourceLabOrFsrCohortQueries,
       TxPvlsBySourceClinicalOrFichaResumoCohortQueries
-          txPvlsBySourceClinicalOrFichaResumoCohortQueries) {
+          txPvlsBySourceClinicalOrFichaResumoCohortQueries,
+      EptsCommonDimension eptsCommonDimension,
+      EptsGeneralIndicator eptsGeneralIndicator,
+      @Qualifier("commonAgeDimensionCohort")
+          AgeDimensionCohortInterface ageDimensionCohortInterface) {
     this.txPvlsBySourceLabOrFsrCohortQueries = txPvlsBySourceLabOrFsrCohortQueries;
     this.txPvlsBySourceClinicalOrFichaResumoCohortQueries =
         txPvlsBySourceClinicalOrFichaResumoCohortQueries;
+    this.eptsCommonDimension = eptsCommonDimension;
+    this.eptsGeneralIndicator = eptsGeneralIndicator;
+    this.ageDimensionCohortInterface = ageDimensionCohortInterface;
   }
 
-  public DataSetDefinition getPvlsLabFsr() {
-    String mappings = "startDate=${startDate},endDate=${endDate},location=${location}";
+  String mappings = "startDate=${startDate},endDate=${endDate},location=${location}";
+  String mappingsKp = "onOrAfter=${startDate},onOrBefore=${endDate},location=${location}";
 
-    CohortIndicatorDataSetDefinition dataSetDefinition = new CohortIndicatorDataSetDefinition();
-    dataSetDefinition.setName("PLF");
-    dataSetDefinition.setDescription("PVLS_LAB_FSR");
-    dataSetDefinition.addParameters(getParameters());
-    return dataSetDefinition;
+  public DataSetDefinition getPvlsLabFsr() {
+    CohortIndicatorDataSetDefinition dsd = new CohortIndicatorDataSetDefinition();
+    dsd.setName("PLF");
+    dsd.setDescription("PVLS_LAB_FSR");
+    dsd.addParameters(getParameters());
+    // Tie dimensions to this data definition
+    dsd.addDimension("gender", EptsReportUtils.map(eptsCommonDimension.gender(), ""));
+    dsd.addDimension(
+        "query", EptsReportUtils.map(eptsCommonDimension.maternityDimension(), mappings));
+    dsd.addDimension(
+        "age",
+        EptsReportUtils.map(
+            eptsCommonDimension.age(ageDimensionCohortInterface), "effectiveDate=${endDate}"));
+    dsd.addDimension(
+        "rt",
+        EptsReportUtils.map(
+            eptsCommonDimension.getViralLoadRoutineTargetReasonsDimension(), mappings));
+    dsd.addDimension(
+        "KP", EptsReportUtils.map(eptsCommonDimension.getKeyPopsDimension(), mappingsKp));
+
+    addNumeratorColumnsForLabAndFsr(dsd, mappings);
+    addDenominatorColumnsForLabAndFsr(dsd, mappings);
+    return dsd;
   }
 
   public DataSetDefinition getPvlFichaMestre() {
-    String mappings = "startDate=${startDate},endDate=${endDate},location=${location}";
-
-    CohortIndicatorDataSetDefinition dataSetDefinition = new CohortIndicatorDataSetDefinition();
-    dataSetDefinition.setName("PFM");
-    dataSetDefinition.setDescription("PVLS_Ficha_Mestre");
-    dataSetDefinition.addParameters(getParameters());
-    return dataSetDefinition;
+    CohortIndicatorDataSetDefinition dsd = new CohortIndicatorDataSetDefinition();
+    dsd.setName("PFM");
+    dsd.setDescription("PVLS_Ficha_Mestre");
+    dsd.addParameters(getParameters());
+    // Tie dimensions to this data definition
+    dsd.addDimension("gender", EptsReportUtils.map(eptsCommonDimension.gender(), ""));
+    dsd.addDimension(
+        "query", EptsReportUtils.map(eptsCommonDimension.maternityDimension(), mappings));
+    dsd.addDimension(
+        "age",
+        EptsReportUtils.map(
+            eptsCommonDimension.age(ageDimensionCohortInterface), "effectiveDate=${endDate}"));
+    dsd.addDimension(
+        "rt",
+        EptsReportUtils.map(
+            eptsCommonDimension.getViralLoadRoutineTargetReasonsDimension(), mappings));
+    dsd.addDimension(
+        "KP", EptsReportUtils.map(eptsCommonDimension.getKeyPopsDimension(), mappingsKp));
+    addDenominatorColumnsForClinicalForms(dsd, mappings);
+    addNumeratorColumnsForClinicalForms(dsd, mappings);
+    return dsd;
   }
+
+  private void addDenominatorColumnsForLabAndFsr(
+      CohortIndicatorDataSetDefinition dsd, String mappings) {}
+
+  private void addNumeratorColumnsForLabAndFsr(
+      CohortIndicatorDataSetDefinition dsd, String mappings) {}
+
+  private void addDenominatorColumnsForClinicalForms(
+      CohortIndicatorDataSetDefinition dsd, String mappings) {}
+
+  private void addNumeratorColumnsForClinicalForms(
+      CohortIndicatorDataSetDefinition dsd, String mappings) {}
 }
