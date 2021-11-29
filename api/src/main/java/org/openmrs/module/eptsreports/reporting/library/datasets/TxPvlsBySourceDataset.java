2e50757223c30b75ffe658a26bd4055ecf91e004
ningosi <nicotwendelee@gmail.com>
2021-11-29 18:16:59 +0300

    Autowire the created cohorts into the datasets ready to create respective data points

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/TxPvlsBySourceDataset.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/TxPvlsBySourceDataset.java
@@ -1,11 +1,29 @@
 package org.openmrs.module.eptsreports.reporting.library.datasets;
 
+import org.openmrs.module.eptsreports.reporting.library.cohorts.TxPvlsBySourceClinicalOrFichaResumoCohortQueries;
+import org.openmrs.module.eptsreports.reporting.library.cohorts.TxPvlsBySourceLabOrFsrCohortQueries;
 import org.openmrs.module.reporting.dataset.definition.CohortIndicatorDataSetDefinition;
 import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TxPvlsBySourceDataset extends BaseDataSet {
+
+  private TxPvlsBySourceLabOrFsrCohortQueries txPvlsBySourceLabOrFsrCohortQueries;
+
+  private TxPvlsBySourceClinicalOrFichaResumoCohortQueries
+      txPvlsBySourceClinicalOrFichaResumoCohortQueries;
+
+  @Autowired
+  public TxPvlsBySourceDataset(
+      TxPvlsBySourceLabOrFsrCohortQueries txPvlsBySourceLabOrFsrCohortQueries,
+      TxPvlsBySourceClinicalOrFichaResumoCohortQueries
+          txPvlsBySourceClinicalOrFichaResumoCohortQueries) {
+    this.txPvlsBySourceLabOrFsrCohortQueries = txPvlsBySourceLabOrFsrCohortQueries;
+    this.txPvlsBySourceClinicalOrFichaResumoCohortQueries =
+        txPvlsBySourceClinicalOrFichaResumoCohortQueries;
+  }
 
   public DataSetDefinition getPvlsLabFsr() {
     String mappings = "startDate=${startDate},endDate=${endDate},location=${location}";
