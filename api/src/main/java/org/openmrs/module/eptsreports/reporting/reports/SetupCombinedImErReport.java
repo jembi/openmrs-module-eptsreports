f2544f44deb538c8c9375db9c1b40e24a1cfa6c2
helderjosue <helderjosuemata@gmail.com>
2021-12-01 11:29:48 +0000

    reactivating report

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupCombinedImErReport.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupCombinedImErReport.java
@@ -13,8 +13,9 @@ import org.openmrs.module.reporting.evaluation.parameter.Mapped;
 import org.openmrs.module.reporting.report.ReportDesign;
 import org.openmrs.module.reporting.report.definition.ReportDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-@Deprecated
+@Component
 public class SetupCombinedImErReport extends EptsDataExportManager {
 
   @Autowired private GenericCohortQueries genericCohortQueries;
