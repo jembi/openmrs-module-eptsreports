ed1734c529b9abd78066d859e59556089aa2936e
helderjosue <helderjosuemata@gmail.com>
2021-12-21 11:39:33 +0000

    Adding concepts and dataset

---
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/PrepNewDataset.java
@@ -0,0 +1,16 @@
+package org.openmrs.module.eptsreports.reporting.library.datasets;
+
+import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
+
+public class PrepNewDataset extends BaseDataSet {
+
+  /**
+   * <b>Description:</b> Constructs PrEP New Dataset
+   *
+   * @return
+   */
+  public DataSetDefinition constructPrepNewDataset() {
+
+    return null;
+  }
+}
