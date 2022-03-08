725283ed1022029f8eeacf2b96ecb2b17aad8cb6
helderjosue <helderjosuemata@gmail.com>
2022-03-08 10:27:07 +0000

    Changing configs to run liveTests

--- api/src/test/java/org/openmrs/module/eptsreports/reporting/intergrated/utils/DefinitionsFGHLiveTest.java
+++ api/src/test/java/org/openmrs/module/eptsreports/reporting/intergrated/utils/DefinitionsFGHLiveTest.java
@@ -1,34 +1,23 @@
 package org.openmrs.module.eptsreports.reporting.intergrated.utils;
 
-import org.junit.Before;
 import org.openmrs.Location;
 import org.openmrs.api.context.Context;
 import org.openmrs.api.context.ContextAuthenticationException;
+import org.openmrs.api.context.UsernamePasswordCredentials;
 import org.openmrs.module.reporting.common.DateUtil;
-import org.openmrs.test.BaseContextSensitiveTest;
 
 public abstract class DefinitionsFGHLiveTest extends DefinitionsTest {
-  /** @see BaseContextSensitiveTest#useInMemoryDatabase() */
-  @Override
-  public Boolean useInMemoryDatabase() {
-    /*
-     * ensure ~/.OpenMRS/openmrs-runtime.properties exists with your properties
-     * such as; connection.username=openmrs
-     * connection.url=jdbc:mysql://127.0.0.1:3316/openmrs
-     * connection.password=wTV.Tpp0|Q&c
-     */
-    return false;
-  }
 
   protected abstract String username();
 
   protected abstract String password();
 
-  @Before
   public void initialize() throws ContextAuthenticationException {
-    Context.authenticate(username(), password());
+    UsernamePasswordCredentials credentials =
+        new UsernamePasswordCredentials(username(), password());
+    Context.authenticate(credentials);
     setStartDate(DateUtil.getDateTime(2013, 2, 6));
     setEndDate(DateUtil.getDateTime(2019, 3, 6));
-    setLocation(new Location(103));
+    setLocation(new Location(220));
   }
 }
