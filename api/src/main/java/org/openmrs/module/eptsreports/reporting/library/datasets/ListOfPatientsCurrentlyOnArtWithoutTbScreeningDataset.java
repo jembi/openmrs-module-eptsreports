aa6a1478834ff505a12876fb366c96929682daf5
eliazmuchave <eliazardomuchave@gmail.com>
2022-05-17 12:08:09 +0200

    Adding list dataset

---
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/ListOfPatientsCurrentlyOnArtWithoutTbScreeningDataset.java
@@ -0,0 +1,14 @@
+package org.openmrs.module.eptsreports.reporting.library.datasets;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ListOfPatientsCurrentlyOnArtWithoutTbScreeningDataset extends BaseDataSet{
+
+    @Autowired
+    public ListOfPatientsCurrentlyOnArtWithoutTbScreeningDataset() {
+
+
+    }
+}
