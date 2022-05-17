c193449fdf7945db39be09372167fae07cdc932a
eliazmuchave <eliazardomuchave@gmail.com>
2022-05-17 12:38:14 +0200

    Adding cohort queries class

---
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/ListOfPatientsCurrentlyOnArtWithoutTbScreenigCohortQueries.java
@@ -0,0 +1,7 @@
+package org.openmrs.module.eptsreports.reporting.library.cohorts;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ListOfPatientsCurrentlyOnArtWithoutTbScreenig {
+}
