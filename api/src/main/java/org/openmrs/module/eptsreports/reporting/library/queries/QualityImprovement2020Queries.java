293b72756d8d370af8f3398da31be5490a01b356
eliazmuchave <eliazardomuchave@gmail.com>
2022-06-15 08:37:50 +0200

    Removing parentesis

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/QualityImprovement2020Queries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/QualityImprovement2020Queries.java
@@ -1948,7 +1948,8 @@ public class QualityImprovement2020Queries {
             + "         AND o.voided = 0     "
             + "         AND o.concept_id = ${856}   "
             + "         AND o.value_numeric >= 1000  "
-            + "         AND ( e.encounter_type = ${6} AND e.encounter_datetime BETWEEN :startDate AND :endDate) "
+            + "         AND e.encounter_type = ${6} "
+            + "         AND e.encounter_datetime BETWEEN :startDate AND :endDate "
             + "         AND e.location_id = :location   "
             + "         GROUP  BY p.patient_id    ";
 
