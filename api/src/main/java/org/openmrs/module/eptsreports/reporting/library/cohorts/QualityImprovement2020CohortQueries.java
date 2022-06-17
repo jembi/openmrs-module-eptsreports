c06ad18b4398c9e003a3f1bf39fd0aad0592e767
helderjosue <helderjosuemata@gmail.com>
2022-06-17 16:28:04 +0000

    disabling dataset columns passing correct mapping period and adjusting pregnancy states query

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/QualityImprovement2020CohortQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/QualityImprovement2020CohortQueries.java
@@ -3,6 +3,7 @@ package org.openmrs.module.eptsreports.reporting.library.cohorts;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.text.StringSubstitutor;
 import org.openmrs.Concept;
+import org.openmrs.EncounterType;
 import org.openmrs.Location;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.eptsreports.metadata.CommonMetadata;
@@ -28,12 +29,7 @@ import org.openmrs.module.reporting.evaluation.parameter.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Component
 public class QualityImprovement2020CohortQueries {
@@ -632,12 +628,20 @@ public class QualityImprovement2020CohortQueries {
    * @param conceptIdAns The value coded answers concept
    * @return CohortDefinition
    */
+  // This will bypass the previous implementation and allow reuse of the method with other
+  // parameters
   public CohortDefinition getPregnantAndBreastfeedingStates(int conceptIdQn, int conceptIdAns) {
+
+    return getPregnantAndBreastfeedingStates(
+        hivMetadata.getMasterCardEncounterType(), conceptIdQn, conceptIdAns);
+  }
+
+  public CohortDefinition getPregnantAndBreastfeedingStates(
+      EncounterType encounterType, int conceptIdQn, int conceptIdAns) {
     Map<String, Integer> map = new HashMap<>();
     map.put("conceptIdQn", conceptIdQn);
     map.put("conceptIdAns", conceptIdAns);
-    map.put(
-        "fichaClinicaEncounterType", hivMetadata.getMasterCardEncounterType().getEncounterTypeId());
+    map.put("fichaClinicaEncounterType", encounterType.getEncounterTypeId());
     StringSubstitutor stringSubstitutor = new StringSubstitutor(map);
     String query =
         "SELECT "
@@ -6482,8 +6486,7 @@ public class QualityImprovement2020CohortQueries {
             SetComparator.IN,
             Arrays.asList(hivMetadata.getAdultoSeguimentoEncounterType()),
             Arrays.asList(
-                hivMetadata.getQuarterlyDispensation(),
-                hivMetadata.getSemiannualDispensation()));
+                hivMetadata.getQuarterlyDispensation(), hivMetadata.getSemiannualDispensation()));
 
     CohortDefinition transferOut = commonCohortQueries.getTranferredOutPatients();
 
