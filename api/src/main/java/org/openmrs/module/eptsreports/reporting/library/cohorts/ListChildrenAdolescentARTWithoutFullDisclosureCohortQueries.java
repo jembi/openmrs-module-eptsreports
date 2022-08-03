a89f1164b669d7c169b2e3a58b04f0c3f178d39a
ningosi <nicotwendelee@gmail.com>
2022-08-03 17:38:14 +0300

    Only have patients without full disclosure from encounter 53

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/ListChildrenAdolescentARTWithoutFullDisclosureCohortQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/ListChildrenAdolescentARTWithoutFullDisclosureCohortQueries.java
@@ -1,6 +1,8 @@
 package org.openmrs.module.eptsreports.reporting.library.cohorts;
 
+import org.apache.commons.text.StringSubstitutor;
 import org.openmrs.Location;
+import org.openmrs.module.eptsreports.metadata.HivMetadata;
 import org.openmrs.module.eptsreports.reporting.library.queries.CommonQueries;
 import org.openmrs.module.eptsreports.reporting.utils.EptsReportUtils;
 import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
@@ -11,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class ListChildrenAdolescentARTWithoutFullDisclosureCohortQueries {
@@ -18,15 +22,18 @@ public class ListChildrenAdolescentARTWithoutFullDisclosureCohortQueries {
   private final GenericCohortQueries genericCohortQueries;
   private final AgeCohortQueries ageCohortQueries;
   private final CommonQueries commonQueries;
+  private final HivMetadata hivMetadata;
 
   @Autowired
   public ListChildrenAdolescentARTWithoutFullDisclosureCohortQueries(
       GenericCohortQueries genericCohortQueries,
       AgeCohortQueries ageCohortQueries,
-      CommonQueries commonQueries) {
+      CommonQueries commonQueries,
+      HivMetadata hivMetadata) {
     this.genericCohortQueries = genericCohortQueries;
     this.ageCohortQueries = ageCohortQueries;
     this.commonQueries = commonQueries;
+    this.hivMetadata = hivMetadata;
   }
 
   public CohortDefinition getBaseCohortForAdolescent() {
@@ -44,11 +51,16 @@ public class ListChildrenAdolescentARTWithoutFullDisclosureCohortQueries {
             ageCohortQueries.createXtoYAgeCohort("age", 8, 14), "effectiveDate=${endDate}"));
     cd.addSearch(
         "art", EptsReportUtils.map(getPatientsOnART(), "endDate=${endDate},location=${location}"));
-    cd.setCompositionString("base AND age AND art");
+    cd.addSearch(
+        "full",
+        EptsReportUtils.map(
+            getAdolescentsCurrentlyOnArtWithFullDisclosure(),
+            "endDate=${endDate},location=${location}"));
+    cd.setCompositionString("(base AND age AND art) AND NOT full");
     return cd;
   }
 
-  public CohortDefinition getPatientsOnART() {
+  private CohortDefinition getPatientsOnART() {
     String query = commonQueries.getARTStartDate(true);
     SqlCohortDefinition cd = new SqlCohortDefinition();
     cd.setName("Get patients on ART");
@@ -60,6 +72,31 @@ public class ListChildrenAdolescentARTWithoutFullDisclosureCohortQueries {
             + query
             + ") art ON p.patient_id=art.patient_id"
             + " WHERE p.voided=0 AND art.first_pickup IS NOT NULL ");
+    return cd;
+  }
+
+  private CohortDefinition getAdolescentsCurrentlyOnArtWithFullDisclosure() {
+    Map<String, Integer> map = new HashMap<>();
+    map.put("53", hivMetadata.getMasterCardEncounterType().getEncounterTypeId());
+    map.put(
+        "6340",
+        hivMetadata.getDisclosureOfHIVDiagnosisToChildrenAdolescentsConcept().getConceptId());
+    map.put("6337", hivMetadata.getRevealdConcept().getConceptId());
+    SqlCohortDefinition cd = new SqlCohortDefinition();
+    cd.setName("Adolescent patients with full disclosure");
+    cd.addParameter(new Parameter("endDate", "End Date", Date.class));
+    cd.addParameter(new Parameter("location", "Location", Location.class));
+    String query =
+        "SELECT p.patient_id FROM patient p "
+            + " INNER JOIN encounter e ON p.patient_id=e.patient_id "
+            + " INNER JOIN obs o ON e.encounter_id=o.encounter_id "
+            + " WHERE p.voided=0 AND e.voided=0 AND o.voided=0 AND e.encounter_type = ${53} "
+            + " AND o.concept_id=${6340} AND o.value_coded= ${6337} AND e.encounter_datetime <= :endDate "
+            + " AND e.location_id=:location ";
+
+    StringSubstitutor sb = new StringSubstitutor(map);
+    String replacedQuery = sb.replace(query);
+    cd.setQuery(replacedQuery);
     return cd;
   }
 }
