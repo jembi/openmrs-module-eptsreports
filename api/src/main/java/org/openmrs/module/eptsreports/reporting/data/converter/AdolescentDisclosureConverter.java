49ea9c3b478d931ac695c8950b4efad430eecbb8
ningosi <nicotwendelee@gmail.com>
2022-09-14 17:43:03 +0300

    Proper manipulation of the observations after getting the last encounter

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/data/converter/AdolescentDisclosureConverter.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/data/converter/AdolescentDisclosureConverter.java
@@ -1,29 +1,58 @@
 package org.openmrs.module.eptsreports.reporting.data.converter;
 
-import org.openmrs.Concept;
+import org.openmrs.Encounter;
 import org.openmrs.Obs;
 import org.openmrs.api.context.Context;
+import org.openmrs.module.eptsreports.metadata.HivMetadata;
 import org.openmrs.module.reporting.data.converter.DataConverter;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Set;
 
 public class AdolescentDisclosureConverter implements DataConverter {
+  String results = "";
+  @Autowired private HivMetadata hivMetadata;
+
   @Override
   public Object convert(Object obj) {
     if (obj == null) {
-      return "Branco";
+      results = "";
     }
-    Obs obs = (Obs) obj;
-    Concept valueCoded = obs.getValueCoded();
-    if (valueCoded == null) {
-      return "Branco";
-    } else if (valueCoded.equals(
-        Context.getConceptService().getConceptByUuid("63e43b2f-801f-412b-87bb-45db8e0ad21b"))) {
-      return "P";
-    } else if (valueCoded.equals(
-        Context.getConceptService().getConceptByUuid("8279b6c1-572d-428c-be45-96e05fe6165d"))) {
-      return "N";
+    Encounter encounter = (Encounter) obj;
+    Set<Obs> getAllObs = encounter.getAllObs();
+    if (getAllObs == null) {
+      results = "";
     }
+    if (!checkIfConceptQuestionWasRecorded(encounter)) {
+      results = "";
+    } else {
+      if (getAllObs != null) {
+        for (Obs obs : getAllObs) {
+          if (obs.getConcept() != null
+              && obs.getConcept()
+                  .equals(hivMetadata.getDisclosureOfHIVDiagnosisToChildrenAdolescentsConcept())) {
 
-    return null;
+            if (obs.getValueCoded() == null) {
+              results = "";
+            } else {
+              if (obs.getValueCoded()
+                  .equals(
+                      Context.getConceptService()
+                          .getConceptByUuid("63e43b2f-801f-412b-87bb-45db8e0ad21b"))) {
+                return "P";
+              } else if (obs.getValueCoded()
+                  .equals(
+                      Context.getConceptService()
+                          .getConceptByUuid("8279b6c1-572d-428c-be45-96e05fe6165d"))) {
+                return "N";
+              }
+            }
+            break;
+          }
+        }
+      }
+    }
+    return results;
   }
 
   @Override
@@ -34,5 +63,20 @@ public class AdolescentDisclosureConverter implements DataConverter {
   @Override
   public Class<?> getDataType() {
     return String.class;
+  }
+
+  private boolean checkIfConceptQuestionWasRecorded(Encounter encounter) {
+
+    boolean isFound = false;
+
+    for (Obs obs : encounter.getAllObs()) {
+      if (obs.getConcept() != null
+          && obs.getConcept()
+              .equals(hivMetadata.getDisclosureOfHIVDiagnosisToChildrenAdolescentsConcept())) {
+        isFound = true;
+        break;
+      }
+    }
+    return isFound;
   }
 }
