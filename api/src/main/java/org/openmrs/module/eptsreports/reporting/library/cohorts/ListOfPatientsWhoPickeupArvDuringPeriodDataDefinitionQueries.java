c3db1b7e6bf89adbd944c57f28873678e4142bd1
helderjosue <helderjosuemata@gmail.com>
2022-11-09 16:07:23 -0200

    correcting state of stay queries and reverting implementation to consider hierarchy

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/ListOfPatientsWhoPickeupArvDuringPeriodDataDefinitionQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/ListOfPatientsWhoPickeupArvDuringPeriodDataDefinitionQueries.java
@@ -1,8 +1,5 @@
 package org.openmrs.module.eptsreports.reporting.library.cohorts;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 import org.apache.commons.text.StringSubstitutor;
 import org.openmrs.Location;
 import org.openmrs.module.eptsreports.metadata.CommonMetadata;
@@ -15,6 +12,10 @@ import org.openmrs.module.reporting.data.patient.definition.SqlPatientDataDefini
 import org.openmrs.module.reporting.evaluation.parameter.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class ListOfPatientsWhoPickeupArvDuringPeriodDataDefinitionQueries {
@@ -90,180 +91,96 @@ public class ListOfPatientsWhoPickeupArvDuringPeriodDataDefinitionQueries {
 
     String query = " ";
     if (lastState) {
-      query =
-          " SELECT patient_id, "
-              + " state "
-              + " FROM   (SELECT s.patient_id, "
-              + " s.state_date, "
-              + " s.state "
-              + " FROM  (SELECT p.patient_id AS patient_id, "
-              + " e.encounter_datetime AS state_date, "
-              + " o.value_coded AS state "
-              + " FROM   patient p "
-              + " INNER JOIN encounter e "
-              + " ON p.patient_id = e.patient_id "
-              + " INNER JOIN obs o "
-              + " ON e.encounter_id = o.encounter_id "
-              + " WHERE  p.voided = 0 "
-              + " AND e.voided = 0 "
-              + " AND o.voided = 0 "
-              + " AND e.encounter_type = ${6} "
-              + " AND e.location_id = :location "
-              + " AND e.encounter_datetime <= :endDate "
-              + " AND o.concept_id = ${6272} "
-              + " AND o.value_coded IN( ${1705}, ${1706}, ${1709}, ${1707}, ${1366}, ${23903}, ${1369} ) "
-              + " UNION "
-              + " SELECT p.patient_id   AS patient_id, "
-              + " o.obs_datetime AS state_date, "
-              + " o.value_coded  AS state "
-              + " FROM   patient p "
-              + " INNER JOIN encounter e "
-              + " ON p.patient_id = e.patient_id "
-              + " INNER JOIN obs o "
-              + " ON e.encounter_id = o.encounter_id "
-              + " WHERE  p.voided = 0 "
-              + " AND e.voided = 0 "
-              + " AND o.voided = 0 "
-              + " AND e.encounter_type = ${53} "
-              + " AND e.location_id = :location "
-              + " AND e.encounter_datetime <= :endDate "
-              + " AND o.concept_id = ${6272} "
-              + " AND o.value_coded IN( ${1705}, ${1706}, ${1709}, ${1707}, ${1366}, ${23903}, ${1369} ) "
-              + " UNION "
-              + " SELECT p.patient_id  AS patient_id, "
-              + " ps.start_date AS state_date, "
-              + " ps.state AS state "
-              + " FROM   patient p "
-              + " INNER JOIN patient_program pg "
-              + " ON p.patient_id = pg.patient_id "
-              + " INNER JOIN program pr "
-              + " ON pr.program_id = pg.program_id "
-              + " INNER JOIN patient_state ps "
-              + " ON pg.patient_program_id = ps.patient_program_id "
-              + " WHERE  ps.state IN( ${state_6}, ${7}, ${8}, ${9}, ${10}, ${29} ) "
-              + " AND pg.location_id = :location "
-              + " AND ps.start_date <= :endDate "
-              + " AND p.voided = 0 "
-              + " AND pg.voided = 0 "
-              + " AND pr.retired = 0 "
-              + " AND ps.voided = 0) s "
-              + " INNER JOIN (SELECT patient_id, "
-              + " Max(state_date) AS state_date "
-              + " FROM   (SELECT p.patient_id AS patient_id, "
-              + " e.encounter_datetime AS state_date, "
-              + " o.value_coded AS state "
-              + " FROM patient p "
-              + " INNER JOIN encounter e "
-              + " ON p.patient_id = e.patient_id "
-              + " INNER JOIN obs o "
-              + " ON "
-              + " e.encounter_id = o.encounter_id "
-              + " WHERE  p.voided = 0 "
-              + " AND e.voided = 0 "
-              + " AND o.voided = 0 "
-              + " AND e.encounter_type = ${6} "
-              + " AND e.location_id = :location "
-              + " AND e.encounter_datetime <= :endDate "
-              + " AND o.concept_id = ${6272} "
-              + " AND o.value_coded IN( ${1705}, ${1706}, ${1709}, ${1707}, ${1366}, ${23903}, ${1369}) "
-              + " UNION "
-              + " SELECT p.patient_id   AS patient_id, "
-              + " o.obs_datetime AS state_date, "
-              + " o.value_coded  AS state "
-              + " FROM   patient p "
-              + " INNER JOIN encounter e "
-              + " ON p.patient_id = e.patient_id "
-              + " INNER JOIN obs o "
-              + " ON "
-              + " e.encounter_id = o.encounter_id "
-              + " WHERE  p.voided = 0 "
-              + " AND e.voided = 0 "
-              + " AND o.voided = 0 "
-              + " AND e.encounter_type = ${53} "
-              + " AND e.location_id = :location "
-              + " AND e.encounter_datetime <= :endDate "
-              + " AND o.concept_id = ${6272} "
-              + " AND o.value_coded IN( ${1705}, ${1706}, ${1709}, ${1707}, ${1366}, ${23903}, ${1369}) "
-              + " UNION "
-              + " SELECT p.patient_id  AS patient_id, "
-              + " ps.start_date AS state_date, "
-              + " ps.state AS state "
-              + " FROM   patient p "
-              + " INNER JOIN patient_program pg "
-              + " ON p.patient_id = pg.patient_id "
-              + " INNER JOIN program pr "
-              + " ON "
-              + " pr.program_id = pg.program_id "
-              + " INNER JOIN patient_state ps "
-              + " ON "
-              + " pg.patient_program_id = ps.patient_program_id "
-              + " WHERE  ps.state IN( ${state_6}, ${7}, ${8}, ${9}, ${10}, ${29} ) "
-              + " AND pg.location_id = :location "
-              + " AND ps.start_date <= :endDate "
-              + " AND p.voided = 0 "
-              + " AND pg.voided = 0 "
-              + " AND pr.retired = 0 "
-              + " AND ps.voided = 0) summary "
-              + " GROUP  BY patient_id) max_date_tbl "
-              + " ON s.patient_id = max_date_tbl.patient_id "
-              + " WHERE  s.state_date = max_date_tbl.state_date)fin ";
+      query += " SELECT p.patient_id, o.value_coded as last_state ";
     } else {
-      query =
-          " SELECT patient_id, "
-              + " Max(state_date) "
-              + " FROM (SELECT p.patient_id AS patient_id, "
-              + " e.encounter_datetime AS state_date, "
-              + " o.value_coded AS state "
-              + " FROM   patient p "
-              + " INNER JOIN encounter e "
-              + " ON p.patient_id = e.patient_id "
-              + " INNER JOIN obs o "
-              + " ON e.encounter_id = o.encounter_id "
-              + " WHERE  p.voided = 0 "
-              + " AND e.voided = 0 "
-              + " AND o.voided = 0 "
-              + " AND e.encounter_type = ${6} "
-              + " AND e.location_id = :location "
-              + " AND e.encounter_datetime <= :endDate "
-              + " AND o.concept_id = ${6272} "
-              + " AND o.value_coded IN( ${1705}, ${1706}, ${1709}, ${1707}, ${1366}, ${23903}, ${1369} ) "
-              + " UNION "
-              + " SELECT p.patient_id   AS patient_id, "
-              + " o.obs_datetime AS state_date, "
-              + " o.value_coded  AS state "
-              + " FROM   patient p "
-              + " INNER JOIN encounter e "
-              + " ON p.patient_id = e.patient_id "
-              + " INNER JOIN obs o "
-              + " ON e.encounter_id = o.encounter_id "
-              + " WHERE  p.voided = 0 "
-              + " AND e.voided = 0 "
-              + " AND o.voided = 0 "
-              + " AND e.encounter_type = ${53} "
-              + " AND e.location_id = :location "
-              + " AND e.encounter_datetime <= :endDate "
-              + " AND o.concept_id = ${6272} "
-              + " AND o.value_coded IN( ${1705}, ${1706}, ${1709}, ${1707}, ${1366}, ${23903}, ${1369} ) "
-              + " UNION "
-              + " SELECT p.patient_id  AS patient_id, "
-              + " ps.start_date AS state_date, "
-              + " ps.state AS state "
-              + " FROM   patient p "
-              + " INNER JOIN patient_program pg "
-              + " ON p.patient_id = pg.patient_id "
-              + " INNER JOIN program pr "
-              + " ON pr.program_id = pg.program_id "
-              + " INNER JOIN patient_state ps "
-              + " ON pg.patient_program_id = ps.patient_program_id "
-              + " WHERE  ps.state IN( ${state_6}, ${7}, ${8}, ${9}, ${10}, ${29} ) "
-              + " AND pg.location_id = :location "
-              + " AND ps.start_date <= :endDate "
-              + " AND p.voided = 0 "
-              + " AND pg.voided = 0 "
-              + " AND pr.retired = 0 "
-              + " AND ps.voided = 0) summary "
-              + " GROUP  BY patient_id ";
+      query +=
+          " SELECT p.patient_id, CASE "
+              + "   WHEN  e.encounter_type = ${53} THEN o.obs_datetime   "
+              + "   WHEN e.encounter_type = ${6} THEN e.encounter_datetime   "
+              + "   END AS state_date ";
     }
+
+    query +=
+        "      FROM patient p  "
+            + "                       INNER JOIN encounter e ON p.patient_id = e.patient_id   "
+            + "                       INNER JOIN obs o ON e.encounter_id = o.encounter_id "
+            + "                       INNER JOIN( "
+            + " SELECT recent.patient_id, MAX(recent.most_recent) as most_recent FROM ( "
+            + getUnionQuery()
+            + " ) recent "
+            + " GROUP BY recent.patient_id "
+            + " )states on p.patient_id = states.patient_id "
+            + " WHERE   "
+            + "  e.voided = 0   "
+            + "  AND p.voided = 0   "
+            + "  AND o.voided = 0   "
+            + "  AND e.location_id = :location "
+            + "AND (   "
+            + "    (   "
+            + "                e.encounter_type = ${53}   "
+            + "            AND        o.concept_id = ${6272}   "
+            + "            AND        o.value_coded IN (${1705}, ${1706}, ${1709}, ${1707}, ${1366}, ${23903}, ${1369})   "
+            + "            AND        o.obs_datetime = states.most_recent   "
+            + "        )   "
+            + "    OR   "
+            + "    (   "
+            + "                e.encounter_type = ${6}   "
+            + "            AND        o.concept_id = ${6273}   "
+            + "            AND        o.value_coded IN (${1705}, ${1706}, ${1709} ,${1707}, ${1366}, ${23903}, ${1369})   "
+            + "            AND        e.encounter_datetime = states.most_recent   "
+            + "            AND NOT EXISTS  (   "
+            + "                SELECT eee.encounter_id FROM encounter eee  "
+            + "                      INNER JOIN obs o3 ON eee.encounter_id = o3.encounter_id   "
+            + "                WHERE eee.patient_id = p.patient_id AND   "
+            + "                        eee.encounter_type = ${53}   "
+            + "                  AND        o3.concept_id = ${6272}   "
+            + "                  AND        o3.value_coded IN (${1705}, ${1706}, ${1709}, ${1707}, ${1366}, ${23903}, ${1369})   "
+            + "                  AND        o3.obs_datetime = states.most_recent   "
+            + "                GROUP BY eee.patient_id   )"
+            + "        )   )"
+            + "GROUP BY p.patient_id "
+            + " UNION ";
+
+    if (lastState) {
+      query += " SELECT pg.patient_id, ps.state as last_state ";
+    } else {
+      query += " SELECT pg.patient_id, ps.start_date as state_date ";
+    }
+
+    query +=
+        " FROM "
+            + " patient_program pg"
+            + "    INNER JOIN patient_state ps"
+            + "        ON pg.patient_program_id = ps.patient_program_id"
+            + "                               INNER JOIN ("
+            + "     SELECT recent.patient_id, MAX(recent.most_recent) as most_recent FROM ( "
+            + getUnionQuery()
+            + " ) recent "
+            + " GROUP BY recent.patient_id "
+            + " )states on pg.patient_id = states.patient_id "
+            + "  WHERE pg.program_id = ${2} "
+            + "  AND ps.state IN (${state_6}, ${7}, ${8}, ${9}, ${10}, ${29} )"
+            + "  AND ps.start_date = states.most_recent"
+            + "  AND pg.voided = 0"
+            + "  AND ps.voided = 0"
+            + "  AND NOT EXISTS ("
+            + "        SELECT ee.encounter_id"
+            + "        FROM encounter ee"
+            + "                 INNER JOIN obs o2 ON ee.encounter_id = o2.encounter_id"
+            + "        WHERE ee.patient_id = pg.patient_id"
+            + "          AND ee.encounter_type = ${6}"
+            + "          AND o2.concept_id = ${6273}"
+            + "          AND o2.value_coded IN (${1705},${1706}, ${1709}, ${1707}, ${1366}, ${23903}, ${1369})"
+            + "          AND o2.obs_datetime = states.most_recent GROUP BY ee.patient_id)"
+            + "  AND NOT EXISTS ( SELECT eee.encounter_id FROM encounter eee"
+            + "                                                    INNER JOIN obs o3 ON eee.encounter_id = o3.encounter_id"
+            + "                   WHERE eee.patient_id = pg.patient_id AND"
+            + "                           eee.encounter_type = ${53}"
+            + "                     AND        o3.concept_id = ${6272}"
+            + "                     AND        o3.value_coded IN (${1705},${1706}, ${1709}, ${1707}, ${1366}, ${23903}, ${1369})"
+            + "                     AND        o3.obs_datetime = states.most_recent"
+            + "                   GROUP BY eee.encounter_id   ) "
+            + " GROUP BY pg.patient_id ";
 
     sqlPatientDataDefinition.setQuery(new StringSubstitutor(valuesMap).replace(query));
 
@@ -315,7 +232,8 @@ public class ListOfPatientsWhoPickeupArvDuringPeriodDataDefinitionQueries {
         "8", hivMetadata.getSuspendedTreatmentWorkflowState().getProgramWorkflowStateId());
     valuesMap.put("1369", commonMetadata.getTransferFromOtherFacilityConcept().getConceptId());
     valuesMap.put("6269", hivMetadata.getActiveOnProgramConcept().getConceptId());
-    valuesMap.put("6", hivMetadata.getPateintActiveArtWorkflowState().getProgramWorkflowStateId());
+    valuesMap.put(
+        "state_6", hivMetadata.getPateintActiveArtWorkflowState().getProgramWorkflowStateId());
     valuesMap.put(
         "29",
         hivMetadata
@@ -374,7 +292,7 @@ public class ListOfPatientsWhoPickeupArvDuringPeriodDataDefinitionQueries {
             + " GROUP BY recent.patient_id "
             + " )states on pg.patient_id = states.patient_id "
             + "  WHERE pg.program_id = ${2} "
-            + "  AND ps.state IN (${6}, ${7}, ${8}, ${9}, ${10}, ${29} )"
+            + "  AND ps.state IN (${state_6}, ${7}, ${8}, ${9}, ${10}, ${29} )"
             + "  AND   ps.start_date = states.most_recent"
             + "  AND pg.voided = 0"
             + "  AND ps.voided = 0"
