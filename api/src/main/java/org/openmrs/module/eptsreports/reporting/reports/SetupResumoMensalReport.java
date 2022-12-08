442d61d1686dbdabd76c42d6b9c4d01dacaa234c
= <eliazardomuchave@gmail.com>
2022-12-08 09:58:58 +0200

    Deprecate report

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupResumoMensalReport.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupResumoMensalReport.java
@@ -28,10 +28,8 @@ import org.openmrs.module.eptsreports.reporting.utils.EptsReportUtils;
 import org.openmrs.module.reporting.ReportingException;
 import org.openmrs.module.reporting.report.ReportDesign;
 import org.openmrs.module.reporting.report.definition.ReportDefinition;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
-@Component
+@Deprecated
 public class SetupResumoMensalReport extends EptsDataExportManager {
 
   private ResumoMensalDataSetDefinition resumoMensalDataSetDefinition;
@@ -40,7 +38,6 @@ public class SetupResumoMensalReport extends EptsDataExportManager {
 
   private ResumoMensalEncounterCountDataSet resumoMensalEncounterCountDataSet;
 
-  @Autowired
   public SetupResumoMensalReport(
       ResumoMensalDataSetDefinition resumoMensalDataSetDefinition,
       GenericCohortQueries genericCohortQueries,
