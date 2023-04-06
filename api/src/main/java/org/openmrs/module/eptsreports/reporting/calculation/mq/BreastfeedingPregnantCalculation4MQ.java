63ee397b35bf235e786231c474ac3f28b6289de2
= <eliazardomuchave@gmail.com>
2023-04-06 11:48:56 +0200

    Truncate hours on pregnancy and breastfeeding date

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/calculation/mq/BreastfeedingPregnantCalculation4MQ.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/calculation/mq/BreastfeedingPregnantCalculation4MQ.java
@@ -5,6 +5,8 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
+import org.joda.time.LocalDate;
 import org.openmrs.EncounterType;
 import org.openmrs.api.context.Context;
 import org.openmrs.calculation.patient.PatientCalculationContext;
@@ -64,8 +66,8 @@ public class BreastfeedingPregnantCalculation4MQ extends AbstractPatientCalculat
     for (Integer ptId : cohort) {
 
       boolean isCandidate = false;
-      Date pregnancyDate = (Date) pregnantDateMap.get(ptId).getValue();
-      Date breastfeedingDate = (Date) breastfeedingDateMap.get(ptId).getValue();
+      Date pregnancyDate = new LocalDate(pregnantDateMap.get(ptId).getValue()).toDate();
+      Date breastfeedingDate =  new LocalDate(breastfeedingDateMap.get(ptId).getValue()).toDate();
       if (state.equals(PregnantOrBreastfeedingWomen.PREGNANTWOMEN)
           && pregnancyDate != null
           && femaleCohort.contains(ptId)
