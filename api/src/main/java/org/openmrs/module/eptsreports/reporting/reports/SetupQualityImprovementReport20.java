ce5067c5dbc216dc267c5af1e09f8fefea3b7537
TinoMuianga <cjrmuianga@gmail.com>
2023-05-29 14:09:19 +0200

    Fix to add on .voided on outer query

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupQualityImprovementReport20.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupQualityImprovementReport20.java
@@ -15,8 +15,9 @@ import org.openmrs.module.reporting.evaluation.parameter.Parameter;
 import org.openmrs.module.reporting.report.ReportDesign;
 import org.openmrs.module.reporting.report.definition.ReportDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-@Deprecated
+@Component
 public class SetupQualityImprovementReport20 extends EptsDataExportManager {
 
   @Autowired protected GenericCohortQueries genericCohortQueries;
