67f083815de97f0681286ed305d6442aa0694fab
TinoMuianga <cjrmuianga@gmail.com>
2023-06-29 15:03:20 +0200

    New Report Creation: Lista Avaliacao MDS

---
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupListOfPatientsWithMDCEvaluation.java
@@ -0,0 +1,100 @@
+package org.openmrs.module.eptsreports.reporting.reports;
+
+import java.io.IOException;
+import java.util.*;
+import org.openmrs.module.eptsreports.reporting.library.datasets.DatimCodeDatasetDefinition;
+import org.openmrs.module.eptsreports.reporting.library.datasets.ListOfPatientsWithMdcevaluationCohortDataset;
+import org.openmrs.module.eptsreports.reporting.library.datasets.SismaCodeDatasetDefinition;
+import org.openmrs.module.eptsreports.reporting.reports.manager.EptsDataExportManager;
+import org.openmrs.module.reporting.ReportingConstants;
+import org.openmrs.module.reporting.ReportingException;
+import org.openmrs.module.reporting.evaluation.parameter.Mapped;
+import org.openmrs.module.reporting.evaluation.parameter.Parameter;
+import org.openmrs.module.reporting.report.ReportDesign;
+import org.openmrs.module.reporting.report.definition.ReportDefinition;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Deprecated
+public class SetupListOfPatientsWithMdcEvaluation extends EptsDataExportManager {
+  @Autowired
+  private ListOfPatientsWithMdcevaluationCohortDataset listOfPatientsWithMdcevaluationCohortDataset;
+
+  @Autowired
+  public SetupListOfPatientsWithMdcEvaluation(
+      ListOfPatientsWithMdcevaluationCohortDataset listOfPatientsWithMdcevaluationCohortDataset) {
+    this.listOfPatientsWithMdcevaluationCohortDataset =
+        listOfPatientsWithMdcevaluationCohortDataset;
+  }
+
+  @Override
+  public String getUuid() {
+    return "51c16c68-1653-11ee-abf0-cbd73a11f3ee";
+  }
+
+  @Override
+  public String getName() {
+    return "List of Patients with MDS Evaluation Cohort Report";
+  }
+
+  @Override
+  public String getDescription() {
+    return "Lista de Pacientes com MDS Evaluation";
+  }
+
+  @Override
+  public String getVersion() {
+    return "1.0-SNAPSHOT";
+  }
+
+  @Override
+  public String getExcelDesignUuid() {
+    return "86268baa-1653-11ee-847b-cfb02184395c";
+  }
+
+  @Override
+  public ReportDefinition constructReportDefinition() {
+    ReportDefinition rd = new ReportDefinition();
+    rd.setUuid(getUuid());
+    rd.setName(getName());
+    rd.setDescription(getDescription());
+    rd.addParameters(getParameters());
+    rd.addDataSetDefinition("DT", Mapped.mapStraightThrough(new DatimCodeDatasetDefinition()));
+    rd.addDataSetDefinition("SM", Mapped.mapStraightThrough(new SismaCodeDatasetDefinition()));
+    rd.addDataSetDefinition(
+        "MDC",
+        Mapped.mapStraightThrough(listOfPatientsWithMdcevaluationCohortDataset.contructDataset()));
+
+    return rd;
+  }
+
+  @Override
+  public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
+    ReportDesign reportDesign = null;
+    try {
+      reportDesign =
+          createXlsReportDesign(
+              reportDefinition,
+              "SESP_MDS_Evaluation_ V1.0 .xls",
+              "List of Patients with MDS Evaluation Cohort Report",
+              getExcelDesignUuid(),
+              null);
+      Properties props = new Properties();
+      props.put("repeatingSections", "sheet:1,row:14,dataset:MDS");
+      props.put("sortWeight", "5000");
+      reportDesign.setProperties(props);
+    } catch (IOException e) {
+      throw new ReportingException(e.toString());
+    }
+
+    return Arrays.asList(reportDesign);
+  }
+
+  @Override
+  public List<Parameter> getParameters() {
+    List<Parameter> parameters = new ArrayList<>();
+    parameters.add(ReportingConstants.START_DATE_PARAMETER);
+    parameters.add(ReportingConstants.END_DATE_PARAMETER);
+    parameters.add(ReportingConstants.LOCATION_PARAMETER);
+    return parameters;
+  }
+}
