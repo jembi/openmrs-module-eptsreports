a91a22ab84a6ddc3f424845c64c5c30473049c99
eliazmuchave <eliazardomuchave@gmail.com>
2023-08-14 11:16:13 +0200

    Add RF 18 - Clients with CD4 count

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/AdvancedDiseaseAndTBCascadeCohortQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/AdvancedDiseaseAndTBCascadeCohortQueries.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.commons.text.StringSubstitutor;
+import org.openmrs.Location;
 import org.openmrs.module.eptsreports.metadata.HivMetadata;
 import org.openmrs.module.eptsreports.metadata.TbMetadata;
 import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
@@ -46,14 +47,14 @@ public class AdvancedDiseaseAndTBCascadeCohortQueries {
    *       Resumo, with the state date falling during the inclusion period.
    * </ul>
    *
-   * *
+   * @return CohortDefinition *
    */
   public CohortDefinition getPatientsWhoReinitiatedArt() {
     SqlCohortDefinition cd = new SqlCohortDefinition();
     cd.setName("Clients who reinitiated ART during the inclusion period");
     cd.addParameter(new Parameter("startDate", "Start Date", Date.class));
     cd.addParameter(new Parameter("endDate", "End Date", Date.class));
-    cd.addParameter(new Parameter("location", "End Date", Date.class));
+    cd.addParameter(new Parameter("location", "End Date", Location.class));
 
     String query =
         "SELECT p.patient_id "
@@ -108,13 +109,77 @@ public class AdvancedDiseaseAndTBCascadeCohortQueries {
     return cd;
   }
 
+  /**
+   *
+   *
+   * <ul>
+   *   <li>Absolute CD4 count results (CD4 Absoluto= ANY RESULT) registered in the Ficha Clinica –
+   *       Mastercard during the inclusion periodor
+   *   <li>CD4 count result (CD4 absoluto - Último CD4= ANY RESULT) registered in the Ficha Resumo –
+   *       Mastercard with the result date during the inclusion period or
+   *   <li>Absolute CD4 count results (CD4 Absoluto= ANY RESULT) registered in Laboratory form
+   *       during the inclusion period or
+   *   <li>Absolute CD4 count results (CD4 Absoluto= ANY RESULT) registered in the Electronic Lab
+   *       form during the inclusion period or
+   *   <li>Absolute CD4 count results (CD4 Absoluto= ANY RESULT) registered in the Ficha DAH with
+   *       the result date during the inclusion period
+   * </ul>
+   *
+   * @return CohortDefinition
+   */
+  public CohortDefinition getPatientsWithCD4Count() {
+
+    SqlCohortDefinition cd = new SqlCohortDefinition();
+    cd.setName("Number of clients with a CD4 count during inclusion period");
+    cd.addParameter(new Parameter("startDate", "Start Date", Date.class));
+    cd.addParameter(new Parameter("endDate", "End Date", Date.class));
+    cd.addParameter(new Parameter("location", "End Date", Location.class));
+
+    String query =
+        "SELECT p.patient_id "
+            + "FROM   patient p "
+            + "       INNER JOIN (SELECT e.patient_id, Max(DATE(e.encounter_datetime)) AS result_date "
+            + "                   FROM   encounter e "
+            + "                          INNER JOIN obs o ON o.encounter_id = e.encounter_id "
+            + "                   WHERE  e.voided = 0 "
+            + "                          AND o.voided = 0 "
+            + "                          AND e.encounter_type IN ( ${6}, ${13}, ${51} ) "
+            + "                          AND o.concept_id = ${1695} "
+            + "                          AND o.value_numeric IS NOT NULL "
+            + "                          AND e.location_id = :location "
+            + "                          AND e.encounter_datetime BETWEEN :startDate AND :endDate "
+            + "                   GROUP  BY e.patient_id "
+            + "                   UNION "
+            + "                   SELECT e.patient_id, Max(o.obs_datetime) AS result_date "
+            + "                   FROM   encounter e "
+            + "                          INNER JOIN obs o ON o.encounter_id = e.encounter_id "
+            + "                   WHERE  e.voided = 0 "
+            + "                          AND o.voided = 0 "
+            + "                          AND e.encounter_type IN ( ${53}, ${90} ) "
+            + "                          AND o.concept_id = ${1695} "
+            + "                          AND o.value_numeric IS NOT NULL "
+            + "                          AND e.location_id = :location "
+            + "                          AND o.obs_datetime BETWEEN :startDate AND :endDate"
+            + "                   GROUP  BY e.patient_id) cd4 "
+            + "               ON cd4.patient_id = p.patient_id "
+            + "WHERE  p.voided = 0 "
+            + "GROUP  BY p.patient_id";
+    StringSubstitutor sb = new StringSubstitutor(getMetadata());
+    cd.setQuery(sb.replace(query));
+    return cd;
+  }
+
   private Map<String, Integer> getMetadata() {
     Map<String, Integer> map = new HashMap<>();
     map.put("6", hivMetadata.getAdultoSeguimentoEncounterType().getEncounterTypeId());
     map.put("53", hivMetadata.getMasterCardEncounterType().getEncounterTypeId());
+    map.put("13", hivMetadata.getMisauLaboratorioEncounterType().getEncounterTypeId());
+    map.put("51", hivMetadata.getFsrEncounterType().getEncounterTypeId());
+    map.put("90", 90);
     map.put("6273", hivMetadata.getStateOfStayOfArtPatient().getConceptId());
     map.put("6272", hivMetadata.getStateOfStayPriorArtPatientConcept().getConceptId());
     map.put("1705", hivMetadata.getRestartConcept().getConceptId());
+    map.put("1695", hivMetadata.getCD4AbsoluteOBSConcept().getConceptId());
     return map;
   }
 }
