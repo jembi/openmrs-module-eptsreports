652885d3ebccdc5ee39c8462db2639f069270c2c
eliazmuchave <eliazardomuchave@gmail.com>
2023-08-14 09:17:19 +0200

    Add RF 15 - Clients who reinitiated ART

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/AdvancedDiseaseAndTBCascadeCohortQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/AdvancedDiseaseAndTBCascadeCohortQueries.java
@@ -1,7 +1,14 @@
 package org.openmrs.module.eptsreports.reporting.library.cohorts;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.commons.text.StringSubstitutor;
 import org.openmrs.module.eptsreports.metadata.HivMetadata;
 import org.openmrs.module.eptsreports.metadata.TbMetadata;
+import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
+import org.openmrs.module.reporting.cohort.definition.SqlCohortDefinition;
+import org.openmrs.module.reporting.evaluation.parameter.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -11,10 +18,103 @@ public class AdvancedDiseaseAndTBCascadeCohortQueries {
   private HivMetadata hivMetadata;
   private TbMetadata tbMetadata;
 
+  private ListOfPatientsArtCohortCohortQueries listOfPatientsArtCohortCohortQueries;
+
+  private TxNewCohortQueries txNewCohortQueries;
+
   @Autowired
-  public AdvancedDiseaseAndTBCascadeCohortQueries(HivMetadata hivMetadata, TbMetadata tbMetadata) {
+  public AdvancedDiseaseAndTBCascadeCohortQueries(
+      HivMetadata hivMetadata,
+      TbMetadata tbMetadata,
+      ListOfPatientsArtCohortCohortQueries listOfPatientsArtCohortCohortQueries,
+      TxNewCohortQueries txNewCohortQueries) {
 
     this.hivMetadata = hivMetadata;
     this.tbMetadata = tbMetadata;
+    this.listOfPatientsArtCohortCohortQueries = listOfPatientsArtCohortCohortQueries;
+    this.txNewCohortQueries = txNewCohortQueries;
+  }
+
+  /**
+   * <b>The system will include all clients who reinitiated ART during the inclusion period who
+   * have:</b>
+   *
+   * <ul>
+   *   <li>The most recent “Mudança de Estado de Permanência” state marked as “Reinício” in Ficha
+   *       Clínica, with the state date falling during the inclusion period.
+   *   <li>OR The most recent “Mudança de Estado de Permanência” state marked as “Reinício” in Ficha
+   *       Resumo, with the state date falling during the inclusion period.
+   * </ul>
+   *
+   * *
+   */
+  public CohortDefinition getPatientsWhoReinitiatedArt() {
+    SqlCohortDefinition cd = new SqlCohortDefinition();
+    cd.setName("Clients who reinitiated ART during the inclusion period");
+    cd.addParameter(new Parameter("startDate", "Start Date", Date.class));
+    cd.addParameter(new Parameter("endDate", "End Date", Date.class));
+    cd.addParameter(new Parameter("location", "End Date", Date.class));
+
+    String query =
+        "SELECT p.patient_id "
+            + "FROM   patient p "
+            + "       INNER JOIN (SELECT e.patient_id "
+            + "                   FROM   encounter e INNER JOIN obs o ON o.encounter_id = e.encounter_id "
+            + "                          INNER JOIN(SELECT e.patient_id, "
+            + "                                            Max(e.encounter_datetime) state_date "
+            + "                                     FROM   encounter e INNER JOIN obs o ON o.encounter_id = e.encounter_id "
+            + "                                     WHERE  e.voided = 0 "
+            + "                                            AND o.voided = 0 "
+            + "                                            AND e.encounter_type = ${6} "
+            + "                                            AND o.concept_id = ${6273} "
+            + "                                            AND e.location_id = :location "
+            + "                                            AND e.encounter_datetime BETWEEN :startDate AND :endDate "
+            + "                                     GROUP  BY e.patient_id) recent_state "
+            + "                                  ON recent_state.patient_id = e.patient_id "
+            + "                   WHERE  e.voided = 0 "
+            + "                          AND o.voided = 0 "
+            + "                          AND o.concept_id = ${6273} "
+            + "                          AND o.value_coded = ${1705} "
+            + "                          AND e.location_id = :location "
+            + "                          AND e.encounter_datetime = recent_state.state_date "
+            + "                   GROUP  BY e.patient_id "
+            + "                   UNION "
+            + "                   SELECT e.patient_id "
+            + "                   FROM   encounter e "
+            + "                          INNER JOIN obs o ON o.encounter_id = e.encounter_id "
+            + "                          INNER JOIN(SELECT e.patient_id, "
+            + "                                            Max(o.obs_datetime) state_date "
+            + "                                     FROM   encounter e INNER JOIN obs o ON o.encounter_id = e.encounter_id "
+            + "                                     WHERE  e.voided = 0 "
+            + "                                            AND o.voided = 0 "
+            + "                                            AND e.encounter_type = ${53} "
+            + "                                            AND o.concept_id = ${6272} "
+            + "                                            AND e.location_id = :location "
+            + "                                            AND o.obs_datetime BETWEEN :startDate AND :endDate "
+            + "                                     GROUP  BY e.patient_id) recent_state ON recent_state.patient_id = e.patient_id "
+            + "                   WHERE  e.voided = 0 "
+            + "                          AND o.voided = 0 "
+            + "                          AND o.concept_id = ${6272} "
+            + "                          AND o.value_coded = ${1705} "
+            + "                          AND e.location_id = :location "
+            + "                          AND o.obs_datetime = recent_state.state_date "
+            + "                   GROUP  BY e.patient_id) reinitiated ON reinitiated.patient_id = p.patient_id "
+            + "WHERE  p.voided = 0 "
+            + "GROUP  BY p.patient_id";
+
+    StringSubstitutor sb = new StringSubstitutor(getMetadata());
+    cd.setQuery(sb.replace(query));
+
+    return cd;
+  }
+
+  private Map<String, Integer> getMetadata() {
+    Map<String, Integer> map = new HashMap<>();
+    map.put("6", hivMetadata.getAdultoSeguimentoEncounterType().getEncounterTypeId());
+    map.put("53", hivMetadata.getMasterCardEncounterType().getEncounterTypeId());
+    map.put("6273", hivMetadata.getStateOfStayOfArtPatient().getConceptId());
+    map.put("6272", hivMetadata.getStateOfStayPriorArtPatientConcept().getConceptId());
+    map.put("1705", hivMetadata.getRestartConcept().getConceptId());
+    return map;
   }
 }
