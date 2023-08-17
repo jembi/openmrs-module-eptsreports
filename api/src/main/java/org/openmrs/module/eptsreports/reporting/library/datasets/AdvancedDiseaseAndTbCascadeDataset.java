909e4df14d54042826b47e68a604d17d0d7a5f53
eliazmuchave <eliazardomuchave@gmail.com>
2023-08-17 11:29:15 +0200

    Add parameters and fields on dataset

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/AdvancedDiseaseAndTbCascadeDataset.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/AdvancedDiseaseAndTbCascadeDataset.java
@@ -1,9 +1,20 @@
 package org.openmrs.module.eptsreports.reporting.library.datasets;
 
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import org.openmrs.Location;
 import org.openmrs.module.eptsreports.reporting.library.cohorts.AdvancedDiseaseAndTBCascadeCohortQueries;
+import org.openmrs.module.eptsreports.reporting.library.cohorts.TXTBCohortQueries;
+import org.openmrs.module.eptsreports.reporting.library.dimensions.AgeDimensionCohortInterface;
+import org.openmrs.module.eptsreports.reporting.library.dimensions.EptsCommonDimension;
+import org.openmrs.module.eptsreports.reporting.library.indicators.EptsGeneralIndicator;
+import org.openmrs.module.eptsreports.reporting.utils.EptsReportUtils;
 import org.openmrs.module.reporting.dataset.definition.CohortIndicatorDataSetDefinition;
 import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
+import org.openmrs.module.reporting.evaluation.parameter.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,11 +23,39 @@ public class AdvancedDiseaseAndTbCascadeDataset extends BaseDataSet {
   @Autowired
   private AdvancedDiseaseAndTBCascadeCohortQueries advancedDiseaseAndTBCascadeCohortQueries;
 
+  @Autowired private EptsGeneralIndicator eptsGeneralIndicator;
+  @Autowired private EptsCommonDimension eptsCommonDimension;
+
+  @Autowired private TXTBCohortQueries txtbCohortQueries;
+
+  @Autowired
+  @Qualifier("commonAgeDimensionCohort")
+  private AgeDimensionCohortInterface ageDimensionCohort;
+
+  private final String reportingPeriod =
+      "startDate=${endDate}-2m,endDate=${generationDate},location=${location}";
+  private final String inclusionPeriod =
+      "startDate=${endDate}-2m,endDate=${endDate-1m},location=${location}";
+
   public DataSetDefinition constructAdvancedDiseaseAndTbCascadeDataset() {
 
     CohortIndicatorDataSetDefinition dataSetDefinition = new CohortIndicatorDataSetDefinition();
     dataSetDefinition.setName("TB7 Dataset");
+    dataSetDefinition.addParameters(getParameters());
+
+    dataSetDefinition.addDimension(
+        "age",
+        EptsReportUtils.map(
+            eptsCommonDimension.age(ageDimensionCohort), "effectiveDate=${endDate}"));
+    dataSetDefinition.addDimension("gender", EptsReportUtils.map(eptsCommonDimension.gender(), ""));
 
     return dataSetDefinition;
+  }
+
+  @Override
+  public List<Parameter> getParameters() {
+    return Arrays.asList(
+        new Parameter("endDate", "endDate", Date.class),
+        new Parameter("location", "Location", Location.class));
   }
 }
