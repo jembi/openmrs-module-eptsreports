6a6b10f72955a6e29867d25c3ec5e8db634c7262
Constantino De Jesus Muianga <57292083+TinoMuianga@users.noreply.github.com>
2023-11-24 14:55:07 +0200

    MEPTS 1348 | TX RTT Changes for December 2023 Release (#1068)
    * Implementing CD4 result less than 200 (not yet implemented exclusion of not eligible for cd4) on TXRTT and Refactor on java doc of TXNEW for CD4Results
    
    * mvn clean install
    
    * Implemented CD4 count and age
    
    * Implemented Patints who ever initiated treatment FR4
    
    * Refactor On CD4 Count/result
    
    * Added string artStartPeriod
    
    * Adding implementation of cd4 desagregations; result greater than 200 and unknown
    
    * Adding java doc on CD4 desagregations
    
    * Added Disagregation of Patients Not Eigible for CD4
    
    * Implemented Returned Patients
    
    * Implementing TX RTT Composition
    
    * Removed Clinical consultation for patient Without Scheduled Drug Pickup Date
    
    * Adding Dataset Columns and rows
    
    * Implemented number of Treatment Interuption Days
    
    * Implementing mappings on template
    
    * Removed concept 23896 CD4 no Inicio de tarv
    
    * Fix on Template and Not eligible for cd4 desagregation
    
    * Fix on returned
    
    * Re-implemented Returned Patients
    
    * Fix on CD4 Result
    
    * Fix on Composition of TX RTT
    
    * Fix on Returned Patients During Period
    
    * Fix on ART start period, changed to previous reporting period
    
    * Fix on Patients Not Eligible for CD4
    
    * removed system.out.println
    
    * Fix on mappings of Art start
    
    * Fix on Mappings and removed unecessary value datetime = not null
    
    * Fixed mappings for cd4
    
    * Fix
    
    * deprecated

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/TxNewCohortQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/TxNewCohortQueries.java
@@ -387,7 +387,7 @@ public class TxNewCohortQueries {
     return cd;
   }
 
-  private CohortDefinition getPatientsWithCd4AndAge(
+  public CohortDefinition getPatientsWithCd4AndAge(
       AdvancedDiseaseAndTBCascadeCohortQueries.Cd4CountComparison cd4,
       Integer minAge,
       Integer maxAge) {
@@ -397,12 +397,12 @@ public class TxNewCohortQueries {
     cd.addParameter(new Parameter("startDate", "Start Date", Date.class));
     cd.addParameter(new Parameter("endDate", "End Date", Date.class));
 
-    String mapping1 = "startDate=${startDate},endDate=${endDate},location=${location}";
-
     CohortDefinition getCd4Result = getCd4Result(cd4);
     CohortDefinition age = ageCohortQueries.createXtoYAgeCohort("Age", minAge, maxAge);
 
-    cd.addSearch("getCd4Result", EptsReportUtils.map(getCd4Result, mapping1));
+    cd.addSearch(
+        "getCd4Result",
+        EptsReportUtils.map(getCd4Result, "endDate=${endDate},location=${location}"));
 
     cd.addSearch("age", EptsReportUtils.map(age, "effectiveDate=${endDate}"));
 
@@ -439,7 +439,6 @@ public class TxNewCohortQueries {
 
     SqlCohortDefinition cd = new SqlCohortDefinition();
     cd.setName("Number of patientes who initiated TARV - Fila and ARV Pickup");
-    cd.addParameter(new Parameter("startDate", "Start Date", Date.class));
     cd.addParameter(new Parameter("endDate", "End Date", Date.class));
     cd.addParameter(new Parameter("location", "Location", Location.class));
 
@@ -535,11 +534,48 @@ public class TxNewCohortQueries {
     return cd;
   }
 
-  /** <b> Patient Disaggregation- CD4 result <200/mm3 </b> */
+  /**
+   * <b>Patient Disaggregation- CD4 result <200/mm3 </b>
+   *
+   * <p>The system will identify patients who are included in the TX_NEW numerator (NEW_FR4) for the
+   * absolute CD4 <200/mm3 disaggregation as follows:
+   *
+   * <ul>
+   *   <li>Patients with an absolute CD4 result <200/mm3 registered in the following sources:
+   * </ul>
+   *
+   * <ul>
+   *   <li>CD4 absolute value at ART initiation marked on Ficha Resumo OR
+   *   <li>Last CD4 absolute value marked on Ficha Resumo OR
+   *   <li>CD4 absolute result marked in the Investigações - Resultados Laboratoriais section on
+   *       Ficha Clínica OR
+   *   <li>CD4 absolute result registered on the Lab Form OR
+   *   <li>CD4 absolute result registered on the e-Lab Form
+   * </ul>
+   *
+   * <p>AND
+   *
+   * <ul>
+   *   <li>Excluding patients <5 years of age (NEW_FR9).
+   * </ul>
+   *
+   * <p>The system will consider the oldest CD4 result date falling between patient ART Start Date -
+   * 90 days and ART Start Date + 28 days from the different sources listed above for the evaluation
+   * of the result (< 200). <br>
+   *
+   * <p><b>Notes:</b>
+   *
+   * <ul>
+   *   <li>For the CD4 at ART initiation registered on Ficha Resumo, the “ART Start Date” that is
+   *       registered on the same Ficha Resumo will be considered as the CD4 result date.
+   *   <li>For clients who have CD4 results ≥200/mm3 and <200/mm3 on the same, oldest date, the CD4
+   *       result <200/mm3 will be prioritized.
+   * </ul>
+   */
   public CohortDefinition getPatientWithCd4ResultLessThan200() {
 
     CompositionCohortDefinition cd = new CompositionCohortDefinition();
-    cd.setName("CD4 Result");
+    cd.setName("Patients with CD4 Result Less than 200");
     cd.addParameter(new Parameter("startDate", "Start Date", Date.class));
     cd.addParameter(new Parameter("endDate", "End Date", Date.class));
     cd.addParameter(new Parameter("location", "location", Location.class));
@@ -549,22 +585,62 @@ public class TxNewCohortQueries {
     CohortDefinition txnew = getTxNewCompositionCohort("patientEnrolledInART");
 
     CohortDefinition cd4Under200 =
-        getCd4Result(
-            AdvancedDiseaseAndTBCascadeCohortQueries.Cd4CountComparison.LessThanOrEqualTo200mm3);
+        getPatientsWithCd4AndAge(
+            AdvancedDiseaseAndTBCascadeCohortQueries.Cd4CountComparison.LessThanOrEqualTo200mm3,
+            5,
+            null);
 
     cd.addSearch("txnew", EptsReportUtils.map(txnew, mapping1));
-    cd.addSearch("cd4Under200", EptsReportUtils.map(cd4Under200, mapping1));
+    cd.addSearch(
+        "cd4Under200", EptsReportUtils.map(cd4Under200, "endDate=${endDate},location=${location}"));
 
     cd.setCompositionString("txnew AND cd4Under200");
 
     return cd;
   }
 
-  /** <b>Patient Disaggregation- CD4 result ≥200/mm3 </b> */
+  /**
+   * <b>Patient Disaggregation- CD4 result ≥200/mm3 </b>
+   *
+   * <p>The system will identify patients who are included in the TX_NEW numerator (NEW_FR4) for the
+   * absolute CD4 ≥200/mm3 disaggregation as follows:
+   *
+   * <ul>
+   *   <li>Patients with an absolute CD4 result <200/mm3 registered in the following sources:
+   * </ul>
+   *
+   * <ul>
+   *   <li>CD4 absolute value at ART initiation marked on Ficha Resumo OR
+   *   <li>Last CD4 absolute value marked on Ficha Resumo OR
+   *   <li>CD4 absolute result marked in the Investigações - Resultados Laboratoriais section on
+   *       Ficha Clínica OR
+   *   <li>CD4 absolute result registered on the Lab Form OR
+   *   <li>CD4 absolute result registered on the e-Lab Form
+   * </ul>
+   *
+   * <p>AND
+   *
+   * <ul>
+   *   <li>Excluding patients <5 years of age (NEW_FR9).
+   * </ul>
+   *
+   * <p>The system will consider the oldest CD4 result date falling between patient ART Start Date -
+   * 90 days and ART Start Date + 28 days from the different sources listed above for the evaluation
+   * of the result (< 200). <br>
+   *
+   * <p><b>Notes:</b>
+   *
+   * <ul>
+   *   <li>For the CD4 at ART initiation registered on Ficha Resumo, the “ART Start Date” that is
+   *       registered on the same Ficha Resumo will be considered as the CD4 result date.
+   *   <li>For clients who have CD4 results ≥200/mm3 and <200/mm3 on the same, oldest date, the CD4
+   *       result <200/mm3 will be prioritized.
+   * </ul>
+   */
   public CohortDefinition getPatientWithcd4ResultGreaterThan200() {
 
     CompositionCohortDefinition cd = new CompositionCohortDefinition();
-    cd.setName("CD4 Result");
+    cd.setName("Patients with CD4 Result Above");
     cd.addParameter(new Parameter("startDate", "Start Date", Date.class));
     cd.addParameter(new Parameter("endDate", "End Date", Date.class));
     cd.addParameter(new Parameter("location", "location", Location.class));
@@ -587,20 +663,36 @@ public class TxNewCohortQueries {
 
     cd.addSearch("txnew", EptsReportUtils.map(txnew, mapping1));
 
-    cd.addSearch("cd4Above200AndAge", EptsReportUtils.map(cd4Above200AndAge, mapping1));
+    cd.addSearch(
+        "cd4Above200AndAge",
+        EptsReportUtils.map(cd4Above200AndAge, "endDate=${endDate},location=${location}"));
 
-    cd.addSearch("cd4Under200AndAge", EptsReportUtils.map(cd4Under200AndAge, mapping1));
+    cd.addSearch(
+        "cd4Under200AndAge",
+        EptsReportUtils.map(cd4Under200AndAge, "endDate=${endDate},location=${location}"));
 
     cd.setCompositionString("(txnew AND cd4Above200AndAge) AND NOT cd4Under200AndAge");
 
     return cd;
   }
 
-  /** <b>Patient Disaggregation- Unknown CD4</b> */
+  /**
+   * <b>Patient Disaggregation- Unknown CD4</b>
+   *
+   * <p>The system will identify patients with unknown CD4 as follows:
+   *
+   * <ul>
+   *   <li>All patients in TX_NEW (NEW_FR4) that are not included in the CD4 result <200/mm3
+   *       (NEW_FR6) nor the CD4 result ≥200/mm3 (NEW_FR7) disaggregates.
+   * </ul>
+   *
+   * <p><b>Note:</b> All children <5 years of age that are included in the TX_NEW numerator will be
+   * included in the “Unknown CD4” disaggregate.
+   */
   public CohortDefinition getPatientWithUnknownCd4Result() {
 
     CompositionCohortDefinition cd = new CompositionCohortDefinition();
-    cd.setName("CD4 Result");
+    cd.setName("Patients with Unknown CD4 Result");
     cd.addParameter(new Parameter("startDate", "Start Date", Date.class));
     cd.addParameter(new Parameter("endDate", "End Date", Date.class));
     cd.addParameter(new Parameter("location", "location", Location.class));
@@ -622,8 +714,12 @@ public class TxNewCohortQueries {
             null);
 
     cd.addSearch("txnew", EptsReportUtils.map(txnew, mapping1));
-    cd.addSearch("cd4Under200AndAge", EptsReportUtils.map(cd4Under200AndAge, mapping1));
-    cd.addSearch("cd4Above200AndAge", EptsReportUtils.map(cd4Above200AndAge, mapping1));
+    cd.addSearch(
+        "cd4Under200AndAge",
+        EptsReportUtils.map(cd4Under200AndAge, "endDate=${endDate},location=${location}"));
+    cd.addSearch(
+        "cd4Above200AndAge",
+        EptsReportUtils.map(cd4Above200AndAge, "endDate=${endDate},location=${location}"));
 
     cd.setCompositionString("txnew AND NOT (cd4Under200AndAge OR cd4Above200AndAge)");
 
