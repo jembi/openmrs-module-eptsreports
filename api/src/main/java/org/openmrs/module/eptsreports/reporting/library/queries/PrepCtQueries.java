d27775499ad1409ebd7b5d9066173c505032ac04
ItsMurumba <kevmurumba@gmail.com>
2023-11-28 16:19:16 +0300

    Removed obs table from   public static String clientsWithAtleastOneFollowupVisitDuringReportingPeriodPrep query

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/PrepCtQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/queries/PrepCtQueries.java
@@ -855,8 +855,7 @@ public class PrepCtQueries {
         "SELECT  p.patient_id "
             + "FROM patient p "
             + "INNER JOIN encounter e ON e.patient_id=p.patient_id "
-            + "INNER JOIN obs o ON o.encounter_id=e.encounter_id "
-            + "WHERE  p.voided = 0 AND e.voided = 0 AND o.voided = 0 "
+            + "WHERE  p.voided = 0 AND e.voided = 0 "
             + "AND e.encounter_type=${81} AND e.encounter_datetime >= :startDate "
             + "AND e.encounter_datetime <= :endDate "
             + "AND e.location_id = :location GROUP  BY p.patient_id";
