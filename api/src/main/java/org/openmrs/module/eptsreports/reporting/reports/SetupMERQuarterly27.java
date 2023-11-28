28c430ce52f3c1cb712e45e0b0b76ee19ace5bd8
ItsMurumba <kevmurumba@gmail.com>
2023-11-28 14:49:28 +0300

    Added Age criteria to getClientsWhoInitiatedPrep

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupMERQuarterly27.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupMERQuarterly27.java
@@ -20,14 +20,14 @@ import java.util.Properties;
 import org.openmrs.module.eptsreports.reporting.library.cohorts.GenericCohortQueries;
 import org.openmrs.module.eptsreports.reporting.library.datasets.*;
 import org.openmrs.module.eptsreports.reporting.reports.manager.EptsDataExportManager;
-import org.openmrs.module.eptsreports.reporting.utils.EptsReportUtils;
 import org.openmrs.module.reporting.ReportingException;
 import org.openmrs.module.reporting.evaluation.parameter.Mapped;
 import org.openmrs.module.reporting.report.ReportDesign;
 import org.openmrs.module.reporting.report.definition.ReportDefinition;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
-@Deprecated
+@Component
 public class SetupMERQuarterly27 extends EptsDataExportManager {
 
   private TxPvlsDataset txPvlsDataset;
@@ -109,27 +109,30 @@ public class SetupMERQuarterly27 extends EptsDataExportManager {
     rd.setName(getName());
     rd.setDescription(getDescription());
     rd.setParameters(txPvlsDataset.getParameters());
-    rd.addDataSetDefinition("N", Mapped.mapStraightThrough(txNewDataset.constructTxNewDataset()));
-    rd.addDataSetDefinition(
-        "C", Mapped.mapStraightThrough(txCurrDataset.constructTxCurrDataset(true)));
-    rd.addDataSetDefinition("P", Mapped.mapStraightThrough(txPvlsDataset.constructTxPvlsDatset()));
-    rd.addDataSetDefinition(
-        "TXML", Mapped.mapStraightThrough(txMlDataset25.constructtxMlDataset()));
-    rd.addDataSetDefinition("R", Mapped.mapStraightThrough(txRttDataset.constructTxRttDataset()));
-    rd.addDataSetDefinition(
-        "T", Mapped.mapStraightThrough(transferredInDataset.constructTransferInDataset()));
-    rd.addDataSetDefinition(
-        "PL", Mapped.mapStraightThrough(txRTTPLHIVDateset.constructTxRTTPLHIVDateset()));
+    //    rd.addDataSetDefinition("N",
+    // Mapped.mapStraightThrough(txNewDataset.constructTxNewDataset()));
+    //    rd.addDataSetDefinition(
+    //        "C", Mapped.mapStraightThrough(txCurrDataset.constructTxCurrDataset(true)));
+    //    rd.addDataSetDefinition("P",
+    // Mapped.mapStraightThrough(txPvlsDataset.constructTxPvlsDatset()));
+    //    rd.addDataSetDefinition(
+    //        "TXML", Mapped.mapStraightThrough(txMlDataset25.constructtxMlDataset()));
+    //    rd.addDataSetDefinition("R",
+    // Mapped.mapStraightThrough(txRttDataset.constructTxRttDataset()));
+    //    rd.addDataSetDefinition(
+    //        "T", Mapped.mapStraightThrough(transferredInDataset.constructTransferInDataset()));
+    //    rd.addDataSetDefinition(
+    //        "PL", Mapped.mapStraightThrough(txRTTPLHIVDateset.constructTxRTTPLHIVDateset()));
     rd.addDataSetDefinition(
         "PREP", Mapped.mapStraightThrough(prepNewDataset.constructPrepNewDataset()));
     rd.addDataSetDefinition("DT", Mapped.mapStraightThrough(new DatimCodeDatasetDefinition()));
-    rd.addDataSetDefinition(
-        "PREPNUM", Mapped.mapStraightThrough(prepCtDataset.constructPrepCtDataset()));
+    //    rd.addDataSetDefinition(
+    //        "PREPNUM", Mapped.mapStraightThrough(prepCtDataset.constructPrepCtDataset()));
 
     // add a base cohort here to help in calculations running
-    rd.setBaseCohortDefinition(
-        EptsReportUtils.map(
-            genericCohortQueries.getBaseCohort(), "endDate=${endDate},location=${location}"));
+    //    rd.setBaseCohortDefinition(
+    //        EptsReportUtils.map(
+    //            genericCohortQueries.getBaseCohort(), "endDate=${endDate},location=${location}"));
 
     return rd;
   }
