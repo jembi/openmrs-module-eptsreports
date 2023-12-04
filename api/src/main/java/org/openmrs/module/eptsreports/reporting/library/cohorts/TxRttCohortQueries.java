f8f3f172ca50c1a899d56227064aa3ab15dc8598
TinoMuianga <cjrmuianga@gmail.com>
2023-12-04 16:04:40 +0200

    Fix on Transferred In, replicated implementation of transferredIn composition to be in accordance of requiriment

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/TxRttCohortQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/TxRttCohortQueries.java
@@ -427,29 +427,27 @@ public class TxRttCohortQueries {
 
     CohortDefinition txCurr = txCurrCohortQueries.getTxCurrCompositionCohort("txCurr", true);
 
-    CohortDefinition transferredOut =
-        commonCohortQueries.getMohTransferredOutPatientsByEndOfPeriod();
+    CohortDefinition transferredOut = getTrfOut();
 
     CohortDefinition homeVisitTrfOut =
         txCurrCohortQueries.getPatientsTransferedOutInLastHomeVisitCard();
 
-    CohortDefinition clinicalVisit = this.getPatientsReturnedTreatmentDuringReportingPeriod();
+    CohortDefinition drugPickUp = getPatientsReturnedTreatmentDuringReportingPeriod();
 
     String mappingsTrfIn = "onOrAfter=${onOrAfter},onOrBefore=${onOrBefore},location=${location}";
     String mappingsCurr = "onOrBefore=${onOrBefore-3m},location=${location}";
-    String mappingsTrfOut = "onOrBefore=${onOrAfter-1d},location=${location}";
+    String mappingsTrfOut = "startDate=${onOrAfter},location=${location}";
     String mappingsHomeVisitTrfOut = "onOrBefore=${onOrAfter-1d},location=${location}";
-    String mappingsClinicalVisit =
+    String mappingsPickUplVisit =
         "startDate=${onOrAfter},endDate=${onOrBefore},location=${location}";
 
     cd.addSearch("transferredIn", EptsReportUtils.map(transferredIn, mappingsTrfIn));
     cd.addSearch("txCurr", EptsReportUtils.map(txCurr, mappingsCurr));
     cd.addSearch("transferredOut", EptsReportUtils.map(transferredOut, mappingsTrfOut));
     cd.addSearch("homeVisitTrfOut", EptsReportUtils.map(homeVisitTrfOut, mappingsHomeVisitTrfOut));
-    cd.addSearch("clinicalVisit", EptsReportUtils.map(clinicalVisit, mappingsClinicalVisit));
+    cd.addSearch("drugPickUp", EptsReportUtils.map(drugPickUp, mappingsPickUplVisit));
 
-    cd.setCompositionString(
-        "(transferredIn OR transferredOut OR homeVisitTrfOut) AND clinicalVisit AND NOT txCurr");
+    cd.setCompositionString("(transferredIn OR transferredOut) AND drugPickUp AND NOT txCurr");
 
     return cd;
   }
@@ -1138,5 +1136,134 @@ public class TxRttCohortQueries {
         "txRtt AND NOT (cd4Under200AndAge OR cd4Above200AndAge OR notEligibleForCd4AndAge)");
 
     return cd;
+  }
+
+  private void addGeneralParameters(CohortDefinition cd) {
+    cd.addParameter(new Parameter("startDate", "Start Date", Date.class));
+    cd.addParameter(new Parameter("location", "Location", Location.class));
+  }
+
+  /**
+   * <b>Technical Specs</b>
+   *
+   * <blockquote>
+   *
+   * <p>The system will identify patients who were transferred out by end of previous reporting
+   * period as follows:
+   *
+   * <ul>
+   *   <li>Patients enrolled on ART Program (Service TARV- Tratamento) with the following last
+   *       status: Transferred Out or
+   *   <li>Patients who have “Mudança no Estado de Permanência TARV” filled out in Ficha Resumo or
+   *       Ficha Clinica – Master Card for the following reasons that are specified in the patient
+   *       chart: patient Transferred Out or
+   *   <li>Patients who have REASON PATIENT MISSED VISIT (MOTIVOS DA FALTA) as “Transferido para
+   *       outra US” or “Auto-transferência” marked in the last Home Visit Card by reporting end
+   *       date. Use the “data da visita” when the patient reason was marked on the home visit card
+   *       as the reference date.
+   * </ul>
+   *
+   * <br>
+   *
+   * <p>The system will identify the most recent date from the different sources as the date of
+   * Transferred Out. Patients who are “marked” as transferred out who have an ARV pick-up
+   * registered in FILA or have a clinical consultation after the date the patient was “marked” as
+   * transferred out will not be considered as Transferred Out.<br>
+   *
+   * <p>The system will consider patient as transferred out as above defined only if the most recent
+   * date between (next scheduled ART pick-up on FILA + 1 day) and (the most recent ART pickup date
+   * on Ficha Recepção – Levantou ARVs + 31 days) falls during the reporting period.
+   *
+   * </blockquote>
+   *
+   * @return {@link CohortDefinition}
+   */
+  public CohortDefinition getTrfOut() {
+    CompositionCohortDefinition definition = new CompositionCohortDefinition();
+    addGeneralParameters(definition);
+    definition.setName("TxTB - Transferred Out");
+    definition.addSearch(
+        "transferred-out",
+        EptsReportUtils.map(
+            txCurrCohortQueries.getPatientsWhoAreTransferredOutToAnotherHf(),
+            "onOrBefore=${startDate-1d},location=${location}"));
+    definition.addSearch(
+        "transferred-out-fila-arv",
+        EptsReportUtils.map(
+            getTrfOutBetweenNextPickupDateFilaAndRecepcaoLevantou(),
+            "startDate=${startDate},location=${location}"));
+    definition.setCompositionString("transferred-out AND transferred-out-fila-arv");
+
+    return definition;
+  }
+
+  /**
+   * The system will consider patient as transferred out as above defined only if the most recent
+   * date between (next scheduled ART pick-up on FILA + 1 day) and (the most recent ART pickup date
+   * on Ficha Recepção – Levantou ARVs + 31 days) falls by end of previous reporting period.
+   *
+   * @return {@link CohortDefinition}
+   */
+  public CohortDefinition getTrfOutBetweenNextPickupDateFilaAndRecepcaoLevantou() {
+
+    SqlCohortDefinition definition = new SqlCohortDefinition();
+    definition.setName(
+        "Patients Transfered Out between (next scheduled ART pick-up on FILA + 1 day) "
+            + "and (the most recent ART pickup date on Ficha Recepção – Levantou ARVs + 31 days");
+
+    definition.addParameter(new Parameter("startDate", "startDate", Date.class));
+    definition.addParameter(new Parameter("location", "location", Location.class));
+
+    Map<String, Integer> valuesMap = new HashMap<>();
+    valuesMap.put("18", hivMetadata.getARVPharmaciaEncounterType().getEncounterTypeId());
+    valuesMap.put("52", hivMetadata.getMasterCardDrugPickupEncounterType().getEncounterTypeId());
+    valuesMap.put("5096", hivMetadata.getReturnVisitDateForArvDrugConcept().getConceptId());
+    valuesMap.put("23866", hivMetadata.getArtDatePickupMasterCard().getConceptId());
+
+    String query =
+        "SELECT final.patient_id FROM  ( "
+            + "SELECT considered_transferred.patient_id, max(considered_transferred.value_datetime) as max_date "
+            + "FROM ( "
+            + "               SELECT     p.patient_id, "
+            + "                          date_add(max(o.value_datetime), interval 1 day) AS value_datetime "
+            + "               FROM       patient p "
+            + "                              INNER JOIN encounter e "
+            + "                                         ON         e.patient_id=p.patient_id "
+            + "                              INNER JOIN obs o "
+            + "                                         ON         o.encounter_id=e.encounter_id "
+            + "               WHERE      p.voided = 0 "
+            + "                 AND        e.voided = 0 "
+            + "                 AND        o.voided = 0 "
+            + "                 AND        e.encounter_type = ${18} "
+            + "                 AND        o.concept_id = ${5096} "
+            + "                 AND        e.encounter_datetime < :startDate "
+            + "                 AND        e.location_id = :location "
+            + "               GROUP BY   p.patient_id "
+            + " UNION "
+            + "               SELECT     p.patient_id, "
+            + "                          date_add(max(o.value_datetime), interval 31 day)  AS value_datetime "
+            + "               FROM       patient p "
+            + "                              INNER JOIN encounter e "
+            + "                                         ON         e.patient_id=p.patient_id "
+            + "                              INNER JOIN obs o "
+            + "                                         ON         o.encounter_id=e.encounter_id "
+            + "               WHERE      p.voided = 0 "
+            + "                 AND        e.voided = 0 "
+            + "                 AND        o.voided = 0 "
+            + "                 AND        e.encounter_type = ${52} "
+            + "                 AND        o.concept_id = ${23866} "
+            + "                 AND        o.value_datetime < :startDate "
+            + "                 AND        e.location_id = :location "
+            + "               GROUP BY   p.patient_id "
+            + " )  considered_transferred "
+            + " GROUP BY considered_transferred.patient_id "
+            + " ) final "
+            + " WHERE final.max_date < :startDate   ";
+
+    StringSubstitutor stringSubstitutor = new StringSubstitutor(valuesMap);
+
+    definition.setQuery(stringSubstitutor.replace(query));
+
+    return definition;
   }
 }
