a7f97e2811aafb0f9c72ef5193ef80b5b02d2a6a
helderjosue <helderjosuemata@gmail.com>
2023-12-22 15:03:41 +0200

    fix: correcting cd4 verificatrion

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/advancedhivillness/ListOfPatientsInAdvancedHivIllnessCohortQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/advancedhivillness/ListOfPatientsInAdvancedHivIllnessCohortQueries.java
@@ -412,7 +412,7 @@ public class ListOfPatientsInAdvancedHivIllnessCohortQueries {
             + "                                       INNER JOIN obs o on e.encounter_id = o.encounter_id "
             + "                         WHERE p.voided = 0 AND e.voided = 0 AND o.voided = 0 "
             + "                           AND e.encounter_type = ${90} "
-            + "                           AND e.encounter_datetime <= :startDate "
+            + "                           AND e.encounter_datetime >= :startDate "
             + "                           AND e.encounter_datetime <= :endDate "
             + "                           AND e.location_id = :location "
             + "                         GROUP BY p.patient_id)followup ON followup.patient_id = e.patient_id "
