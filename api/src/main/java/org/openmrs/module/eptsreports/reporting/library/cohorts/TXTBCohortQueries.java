6fae003faba8a7f913f557f300415df6637c6160
helderjosue <helderjosuemata@gmail.com>
2024-03-01 09:32:40 +0200

    feat: ading missing criteria for transferredOut

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/TXTBCohortQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/TXTBCohortQueries.java
@@ -1297,7 +1297,7 @@ public class TXTBCohortQueries {
         "(art-list AND (tb-screening OR tb-investigation OR tb-investigation-negative OR started-tb-treatment OR in-tb-program OR pulmonary-tb OR marked-as-tb-treatment-start "
             + "OR (tuberculosis-symptomys OR active-tuberculosis OR tb-observations OR application-for-laboratory-research OR tb-genexpert-test OR tb-genexpert-lab-test OR tb-xpert-mtb OR culture-test OR culture-test-lab "
             + "OR test-tb-lam OR test-tb-lam-lab OR test-bk OR x-ray-chest) OR result-for-basiloscopia)) "
-            + "NOT ((transferred-out NOT (started-tb-treatment OR in-tb-program)) OR started-tb-treatment-previous-period OR in-tb-program-previous-period OR pulmonary-tb-date OR marked-as-tratamento-tb-inicio)");
+            + "NOT ((transferred-out NOT (marked-as-tb-treatment-start OR started-tb-treatment OR pulmonary-tb OR in-tb-program)) OR started-tb-treatment-previous-period OR in-tb-program-previous-period OR pulmonary-tb-date OR marked-as-tratamento-tb-inicio)");
 
     return definition;
   }
@@ -2451,7 +2451,7 @@ public class TXTBCohortQueries {
       Concept mtbTest,
       Concept positive,
       Concept yes) {
-
+    // genxpert both sources
     CohortDefinition genexpertTestCohort =
         genericCohortQueries.generalSql(
             "genexpertTestCohort",
@@ -2466,8 +2466,7 @@ public class TXTBCohortQueries {
                 laboratory, genexpertTest, Arrays.asList(positive)));
     addGeneralParameters(genexpertLabTestCohort);
 
-    CohortDefinition basiloscopiaExamCohort = getSmearMicroscopyOnlyPositiveResult();
-
+    CohortDefinition basiloscopiaExamCohort = getSmearMicroscopyOnlyPositiveResult(true);
     CohortDefinition tbLamTestCohort =
         genericCohortQueries.generalSql(
             "tbLamTestCohort",
