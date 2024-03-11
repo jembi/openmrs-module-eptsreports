a010f9bf4e3c0fe9706e217f4b538798038a9988
Constantino De Jesus Muianga <57292083+TinoMuianga@users.noreply.github.com>
2024-03-11 11:24:53 +0200

    MEPTS-1433 | Resumo Mensal HIV - Diff FGH DB (#1148)
    * Re-implemented Transferred out query for B12
    
    * Fix on Transferred Out Implementation
    
    * Transferred_out re-implemented
    
    * Fix on Transferred_out to check data masi recente dentre a do proximo levantamento e do agendamento, sendo essa data posterior a “Data Fim do Relatório”
    
    * Removed Duplicated implementaion of Transferred_out
    
    * Merged with 2.x and fix on basecohort

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/IntensiveMonitoringCohortQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/IntensiveMonitoringCohortQueries.java
@@ -4044,7 +4044,7 @@ public class IntensiveMonitoringCohortQueries {
     sqlCohortDefinition.addParameter(new Parameter("startDate", "Start Date", Date.class));
     sqlCohortDefinition.addParameter(new Parameter("endDate", "end Date", Date.class));
     sqlCohortDefinition.addParameter(
-            new Parameter("revisionEndDate", "revisionEndDate", Date.class));
+        new Parameter("revisionEndDate", "revisionEndDate", Date.class));
     sqlCohortDefinition.addParameter(new Parameter("location", "location", Location.class));
 
     Map<String, Integer> map = new HashMap<>();
@@ -4053,38 +4053,38 @@ public class IntensiveMonitoringCohortQueries {
     map.put("1267", hivMetadata.getCompletedConcept().getConceptId());
 
     String query =
-            "SELECT p.patient_id "
-                    + "FROM   patient p "
-                    + "       INNER JOIN (SELECT e.patient_id, "
-                    + "                          MAX(e.encounter_datetime) AS most_recent "
-                    + "                   FROM   encounter e "
-                    + "                   WHERE  e.voided = 0 "
-                    + "                          AND e.location_id = :location "
-                    + "                          AND e.encounter_type = ${6} "
-                    + "                          AND e.encounter_datetime >= :startDate "
-                    + "                          AND e.encounter_datetime <= :endDate "
-                    + "                   GROUP  BY e.patient_id) last_consultation "
-                    + "               ON last_consultation.patient_id = p.patient_id "
-                    + "       INNER JOIN (SELECT p.patient_id, "
-                    + "                          MAX(o.obs_datetime) AS last_tb_treatment "
-                    + "                   FROM   patient p "
-                    + "                          INNER JOIN encounter e "
-                    + "                                  ON e.patient_id = p.patient_id "
-                    + "                          INNER JOIN obs o "
-                    + "                                  ON o.encounter_id = e.encounter_id "
-                    + "                   WHERE  p.voided = 0 "
-                    + "                          AND e.voided = 0 "
-                    + "                          AND o.voided = 0 "
-                    + "                          AND e.location_id = :location "
-                    + "                          AND e.encounter_type = ${6} "
-                    + "                          AND o.concept_id = ${1268} "
-                    + "                          AND o.value_coded = ${1267} "
-                    + "                          AND e.encounter_datetime <= :revisionEndDate "
-                    + "                   GROUP  BY p.patient_id) finished_treatment "
-                    + "               ON finished_treatment.patient_id = p.patient_id "
-                    + "WHERE  p.voided = 0 "
-                    + "       AND TIMESTAMPDIFF(DAY, finished_treatment.last_tb_treatment, "
-                    + "               last_consultation.most_recent) <= 30";
+        "SELECT p.patient_id "
+            + "FROM   patient p "
+            + "       INNER JOIN (SELECT e.patient_id, "
+            + "                          MAX(e.encounter_datetime) AS most_recent "
+            + "                   FROM   encounter e "
+            + "                   WHERE  e.voided = 0 "
+            + "                          AND e.location_id = :location "
+            + "                          AND e.encounter_type = ${6} "
+            + "                          AND e.encounter_datetime >= :startDate "
+            + "                          AND e.encounter_datetime <= :endDate "
+            + "                   GROUP  BY e.patient_id) last_consultation "
+            + "               ON last_consultation.patient_id = p.patient_id "
+            + "       INNER JOIN (SELECT p.patient_id, "
+            + "                          MAX(o.obs_datetime) AS last_tb_treatment "
+            + "                   FROM   patient p "
+            + "                          INNER JOIN encounter e "
+            + "                                  ON e.patient_id = p.patient_id "
+            + "                          INNER JOIN obs o "
+            + "                                  ON o.encounter_id = e.encounter_id "
+            + "                   WHERE  p.voided = 0 "
+            + "                          AND e.voided = 0 "
+            + "                          AND o.voided = 0 "
+            + "                          AND e.location_id = :location "
+            + "                          AND e.encounter_type = ${6} "
+            + "                          AND o.concept_id = ${1268} "
+            + "                          AND o.value_coded = ${1267} "
+            + "                          AND e.encounter_datetime <= :revisionEndDate "
+            + "                   GROUP  BY p.patient_id) finished_treatment "
+            + "               ON finished_treatment.patient_id = p.patient_id "
+            + "WHERE  p.voided = 0 "
+            + "       AND TIMESTAMPDIFF(DAY, finished_treatment.last_tb_treatment, "
+            + "               last_consultation.most_recent) <= 30";
 
     StringSubstitutor stringSubstitutor = new StringSubstitutor(map);
 
