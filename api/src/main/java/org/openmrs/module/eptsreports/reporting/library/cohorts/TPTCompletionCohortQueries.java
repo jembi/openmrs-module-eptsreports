0362a64b37e6fce44e9f4a49c4261667c745026a
Hélder Josué <55550145+helderjosue@users.noreply.github.com>
2024-04-22 09:46:26 +0200

    Update api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/TPTCompletionCohortQueries.java
    Co-authored-by: Paulo Sérgio Jaime Matsinhe <psmatsinhe@gmail.com>

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/TPTCompletionCohortQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/TPTCompletionCohortQueries.java
@@ -2453,7 +2453,6 @@ public class TPTCompletionCohortQueries {
             + "WHERE e2.voided = 0 "
             + "       AND e2.patient_id = p.patient_id "
             + "       AND e2.encounter_type = ${60} "
-            + "       AND e2.location_id = :location "
             + "       AND oo.concept_id = ${23986} "
             + "       AND oo.value_coded = ${23720} "
             + "       AND e2.encounter_datetime BETWEEN tabela.start_date AND Date_add(tabela.start_date, INTERVAL 5 month) "
