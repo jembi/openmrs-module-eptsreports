77559bb1143cd0416aac0a7af2be501e55f6989f
helderjosue <helderjosuemata@gmail.com>
2024-06-12 20:58:01 +0200

    feat: adding columns from f to i

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/list/ListOfChildrenEnrolledInCCRDataset.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/list/ListOfChildrenEnrolledInCCRDataset.java
@@ -1,18 +1,17 @@
 package org.openmrs.module.eptsreports.reporting.library.datasets.list;
 
 import org.openmrs.PatientIdentifierType;
+import org.openmrs.PersonAttributeType;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.eptsreports.reporting.data.converter.GenderConverter;
+import org.openmrs.module.eptsreports.reporting.library.cohorts.ListOfPatientsDefaultersOrIITCohortQueries;
 import org.openmrs.module.eptsreports.reporting.library.cohorts.advancedhivillness.ListOfPatientsInAdvancedHivIllnessCohortQueries;
 import org.openmrs.module.eptsreports.reporting.library.cohorts.ccr.ListOfChildrenEnrolledInCCRDataDefinitionQueries;
 import org.openmrs.module.eptsreports.reporting.library.datasets.BaseDataSet;
 import org.openmrs.module.reporting.data.DataDefinition;
 import org.openmrs.module.reporting.data.converter.DataConverter;
 import org.openmrs.module.reporting.data.converter.ObjectFormatter;
-import org.openmrs.module.reporting.data.person.definition.ConvertedPersonDataDefinition;
-import org.openmrs.module.reporting.data.person.definition.GenderDataDefinition;
-import org.openmrs.module.reporting.data.person.definition.PersonIdDataDefinition;
-import org.openmrs.module.reporting.data.person.definition.PreferredNameDataDefinition;
+import org.openmrs.module.reporting.data.person.definition.*;
 import org.openmrs.module.reporting.dataset.definition.CohortIndicatorDataSetDefinition;
 import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
 import org.openmrs.module.reporting.dataset.definition.PatientDataSetDefinition;
@@ -27,17 +26,21 @@ public class ListOfChildrenEnrolledInCCRDataset extends BaseDataSet {
 
   private final ListOfPatientsInAdvancedHivIllnessCohortQueries
       listOfPatientsInAdvancedHivIllnessCohortQueries;
+  private final ListOfPatientsDefaultersOrIITCohortQueries
+      listOfPatientsDefaultersOrIITCohortQueries;
 
   @Autowired
   public ListOfChildrenEnrolledInCCRDataset(
       ListOfChildrenEnrolledInCCRDataDefinitionQueries
           listOfChildrenEnrolledInCCRDataDefinitionQueries,
       ListOfPatientsInAdvancedHivIllnessCohortQueries
-          listOfPatientsInAdvancedHivIllnessCohortQueries) {
+          listOfPatientsInAdvancedHivIllnessCohortQueries,
+      ListOfPatientsDefaultersOrIITCohortQueries listOfPatientsDefaultersOrIITCohortQueries) {
     this.listOfChildrenEnrolledInCCRDataDefinitionQueries =
         listOfChildrenEnrolledInCCRDataDefinitionQueries;
     this.listOfPatientsInAdvancedHivIllnessCohortQueries =
         listOfPatientsInAdvancedHivIllnessCohortQueries;
+    this.listOfPatientsDefaultersOrIITCohortQueries = listOfPatientsDefaultersOrIITCohortQueries;
   }
 
   public DataSetDefinition listOfChildrenEnrolledInCCRColumnsDataset() {
@@ -49,6 +52,16 @@ public class ListOfChildrenEnrolledInCCRDataset extends BaseDataSet {
     PatientIdentifierType identifierType =
         Context.getPatientService()
             .getPatientIdentifierTypeByUuid("e2b966d0-1d5f-11e0-b929-000c29ad1d07");
+
+    PersonAttributeType contactAttributeType =
+        Context.getPersonService()
+            .getPersonAttributeTypeByUuid("e2e3fd64-1d5f-11e0-b929-000c29ad1d07");
+
+    DataDefinition conctactDef =
+        new ConvertedPersonDataDefinition(
+            "contact",
+            new PersonAttributeDataDefinition(
+                contactAttributeType.getName(), contactAttributeType));
 
     DataConverter nameFormatter = new ObjectFormatter("{givenName} {middleName} {familyName} ");
 
@@ -73,19 +86,40 @@ public class ListOfChildrenEnrolledInCCRDataset extends BaseDataSet {
 
     // Sex -Sheet 1: Column C
     patientDataSetDefinition.addColumn(
-            "gender", new GenderDataDefinition(), "", new GenderConverter());
+        "gender", new GenderDataDefinition(), "", new GenderConverter());
 
-//    Age (Idade) – Sheet 1: Column D - In years
+    //    Age (Idade) – Sheet 1: Column D - In years
     patientDataSetDefinition.addColumn(
-            "age_years",
-            listOfChildrenEnrolledInCCRDataDefinitionQueries.getPatientAgeInYearsOrMonths(true),
-            "endDate=${endDate}");
+        "age_years",
+        listOfChildrenEnrolledInCCRDataDefinitionQueries.getPatientAgeInYearsOrMonths(true),
+        "endDate=${endDate}");
 
-//    Age (Idade) – Sheet 1: Column E - In months
+    //    Age (Idade) – Sheet 1: Column E - In months
     patientDataSetDefinition.addColumn(
-            "age_months",
-            listOfChildrenEnrolledInCCRDataDefinitionQueries.getPatientAgeInYearsOrMonths(false),
-            "endDate=${endDate}");
+        "age_months",
+        listOfChildrenEnrolledInCCRDataDefinitionQueries.getPatientAgeInYearsOrMonths(false),
+        "endDate=${endDate}");
+
+    // Address (Localidade) - Sheet 1: Column F
+    patientDataSetDefinition.addColumn(
+        "location",
+        listOfPatientsDefaultersOrIITCohortQueries.getLocation(),
+        "location=${location}");
+
+    // Address (Bairro) – Sheet 1: Column G
+    patientDataSetDefinition.addColumn(
+        "neighborhood",
+        listOfPatientsDefaultersOrIITCohortQueries.getNeighborhood(),
+        "location=${location}");
+
+    // Address (Ponto de Referencia) – Sheet 1: Column H
+    patientDataSetDefinition.addColumn(
+        "reference_point",
+        listOfPatientsDefaultersOrIITCohortQueries.getReferencePoint(),
+        "location=${location}");
+
+    //    Contacto – Sheet 1: Column I
+    patientDataSetDefinition.addColumn("contact", conctactDef, "");
 
     return patientDataSetDefinition;
   }
