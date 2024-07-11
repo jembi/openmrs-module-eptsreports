cb67d74fd001f0e04c6186a2d1739c83c515783c
TinoMuianga <cjrmuianga@gmail.com>
2024-07-11 16:31:58 +0200

    Implemented the new .xls template

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupAdvancedDiseaseAndTbCascadeReport.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/reports/SetupAdvancedDiseaseAndTbCascadeReport.java
@@ -80,7 +80,7 @@ public class SetupAdvancedDiseaseAndTbCascadeReport extends EptsDataExportManage
       reportDesign =
           createXlsReportDesign(
               reportDefinition,
-              "Template_TB7 Relatorio da Cascata de DAH e TB_v12.xls",
+              "SESP TB7__Relatorio_da_Cascata_de_Doenca_Avancada_por_HIV_e_TB_Template_v.2.5.xls",
               "TB7: Relatório Cascata de Doença Avançada por HIV e TB",
               getExcelDesignUuid(),
               null);
