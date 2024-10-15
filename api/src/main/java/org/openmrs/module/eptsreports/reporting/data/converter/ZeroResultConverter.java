33621d58f0c5334c29621edce42625d5afa4c018
Constantino De Jesus Muianga <57292083+TinoMuianga@users.noreply.github.com>
2024-10-15 09:16:31 +0200

    MEPTS - 1595 | MDS Report Diff in FGH DB (#1292)
    * Implemented B, C and D 7 - Condição Clínica Activa de Estadio III ou IV
    
    * Added concepts metadata
    
    * Implemented B C and D 12 Planeamento Familiar
    
    * Implemented B C and D 13 Columns
    
    * Implemented B C and D 15 Columns and added new concepts metadata
    
    * Implemented B C and D 18 Columns
    
    * Implemented B C and D 19 Columns
    
    * Updated and mapped the New MDS Template
    
    * Added new Dataset for Diferent MDS cohorts
    
    * Updated the Evaluation start and end date periods
    
    * Updated Template
    
    * Fix on query alias
    
    * Fix on query alias on b8
    
    * Reimplemented MDS dataset and added chort 24 to 36 and 36 month string queries
    
    * Fix on Months Cohort string queries
    
    * Implemented the Patints by Year Cohort string query
    
    * Fix on Patient Year Cohort string query
    
    * Added Cohort period for B C and D 4 to 9
    
    * Fix on Pregnant or Breastfeeding query
    
    * Added Cohort period for B C and D 5 to 10
    
    * Added Cohort period for B C and D 11 to 19
    
    * Implemented B C and D 20 last state of stay
    
    * Implemented the Cohort period for B C D 20 Last State of Stay
    
    * Fix on MDS end 3 added concept map
    
    * Fix on Last state of stay Trf Out
    
    * Fix on Cohort Period mappings in all sections
    
    * Fix on Cohort Period mappings on Section B C and D 20
    
    * Fix on MDS date query
    
    * Fix on A7, C1 and C2 implementation cohort periods
    
    * Fix on B6 implementation
    
    * Fix on B C D 12 - Family Planing implementation
    
    * Fixed B C D 13 - Received tpt implementation
    
    * Fixed B C D 15 - Arterial Pressure implementation
    
    * Fix on Nao result for Patients with no Positive CACUM
    
    * Results Harmonization
    
    * Added converter to give 0 result instead of empty on number of Consultations
    
    * Fix on Estado De Permanencia - Abandono
    
    * Fix on A6 indicator converter to answer Yes or No

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/data/converter/ZeroResultConverter.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/data/converter/ZeroResultConverter.java
@@ -2,23 +2,23 @@ package org.openmrs.module.eptsreports.reporting.data.converter;
 
 import org.openmrs.module.reporting.data.converter.DataConverter;
 
-public class YesOrNoConverter implements DataConverter {
+public class ZeroResultConverter implements DataConverter {
 
   @Override
-  public Object convert(Object o) {
-    if (o == null) {
-      return "Não";
+  public Object convert(Object obj) {
+    if (obj == null) {
+      return "0";
     }
-    return "Sim";
+    return obj.toString();
   }
 
   @Override
   public Class<?> getInputDataType() {
-    return null;
+    return String.class;
   }
 
   @Override
   public Class<?> getDataType() {
-    return null;
+    return String.class;
   }
 }
