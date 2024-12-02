91a4f4da27dda5d8841fe6a623009d59d434c09a
TinoMuianga <cjrmuianga@gmail.com>
2024-12-02 17:27:16 +0200

    Added new setup dataset and cohort queries files

---
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/datasets/resumo/ResumoMensalCcrDataSetDefinition.java
@@ -0,0 +1,64 @@
+/*
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+package org.openmrs.module.eptsreports.reporting.library.datasets.resumo;
+
+import static org.openmrs.module.eptsreports.reporting.utils.EptsReportUtils.map;
+
+import org.openmrs.module.eptsreports.reporting.library.cohorts.ResumoMensalCcrCohortQueries;
+import org.openmrs.module.eptsreports.reporting.library.datasets.BaseDataSet;
+import org.openmrs.module.eptsreports.reporting.library.dimensions.AgeDimensionCohortInterface;
+import org.openmrs.module.eptsreports.reporting.library.dimensions.EptsCommonDimension;
+import org.openmrs.module.eptsreports.reporting.library.indicators.EptsGeneralIndicator;
+import org.openmrs.module.reporting.dataset.definition.CohortIndicatorDataSetDefinition;
+import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ResumoMensalCcrDataSetDefinition extends BaseDataSet {
+
+  private EptsCommonDimension eptsCommonDimension;
+
+  private EptsGeneralIndicator eptsGeneralIndicator;
+
+  private ResumoMensalCcrCohortQueries resumoMensalCcrCohortQueries;
+
+  @Autowired
+  @Qualifier("commonAgeDimensionCohort")
+  private AgeDimensionCohortInterface ageDimensionCohort;
+
+  @Autowired
+  public ResumoMensalCcrDataSetDefinition(
+      EptsCommonDimension eptsCommonDimension,
+      EptsGeneralIndicator eptsGeneralIndicator,
+      ResumoMensalCcrCohortQueries resumoMensalCcrCohortQueries) {
+    this.eptsCommonDimension = eptsCommonDimension;
+    this.eptsGeneralIndicator = eptsGeneralIndicator;
+    this.resumoMensalCcrCohortQueries = resumoMensalCcrCohortQueries;
+  }
+
+  public DataSetDefinition constructResumoMensalDataset() {
+    CohortIndicatorDataSetDefinition dsd = new CohortIndicatorDataSetDefinition();
+    String mapping = "startDate=${startDate},endDate=${endDate},location=${location}";
+    dsd.setName("Resumo Mensal CCR Dataset");
+    dsd.addParameters(getParameters());
+
+    dsd.addDimension("gender", map(eptsCommonDimension.gender(), ""));
+    dsd.addDimension(
+        "age", map(eptsCommonDimension.age(ageDimensionCohort), "effectiveDate=${endDate}"));
+
+    return dsd;
+  }
+}
