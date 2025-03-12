8bc9e3bc3fa1f2eafa5e6d3ddf2956228b117fc7
TinoMuianga <constantino.muianga@jembi.org>
2025-03-12 11:56:18 +0200

    Fix on INH End on FICHA RESUMO to check INH Start date

--- api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/TPTCompletionCohortQueries.java
+++ api/src/main/java/org/openmrs/module/eptsreports/reporting/library/cohorts/TPTCompletionCohortQueries.java
@@ -2769,14 +2769,19 @@ public class TPTCompletionCohortQueries {
             + " INNER JOIN obs o ON e.encounter_id = o.encounter_id "
             + " INNER JOIN obs o2 ON e.encounter_id = o2.encounter_id "
             + " INNER JOIN ( "
+            + "     SELECT inh.patient_id, "
+            + "     MIN(inh.start_date) AS inh_start_date "
+            + "     FROM ( "
             + Y
+            + "     GROUP BY inh.patient_id "
+            + "     ) inh "
             + " ) inh_start ON inh_start.patient_id = p.patient_id "
             + "       WHERE p.voided =0 AND e.voided = 0 AND o.voided=0 AND o2.voided=0 "
             + "             AND e.encounter_type = ${53} AND e.location_id = :location "
             + "             AND  (o.concept_id = ${23985} AND o.value_coded = ${656}) "
             + "             AND (o2.concept_id = ${165308} AND o2.value_coded = ${1267}) "
-            + "             AND o2.obs_datetime BETWEEN DATE_ADD(inh_start.start_date, interval 173 day) "
-            + "            AND DATE_ADD(inh_start.start_date, interval 365 day) "
+            + "             AND o2.obs_datetime BETWEEN DATE_ADD(inh_start.inh_start_date, interval 173 day) "
+            + "            AND DATE_ADD(inh_start.inh_start_date, interval 365 day) "
             + "           AND o2.obs_datetime <= :endDate "
             + " GROUP BY p.patient_id ";
 
